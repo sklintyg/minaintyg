c5bf0cb59d521c5d91026e364f62fa0aeecdc91c
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-04-18 16:36:23 +0200

    Initial maven module for certificate domain model

---
+++ model/src/main/java/se.inera.certificate.model/Id.java
@@ -0,0 +1,23 @@
+package se.inera.certificate.model;
+
+public class Id {
+
+    private String extension;
+    private String root;
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+    }
+}
