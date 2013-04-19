c15cf15c7181bfd14b5d193b547ea51c15271a4b
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-04-19 16:11:40 +0200

    Initial version of CertificateService with listCertificates and getCertificate methods

---
+++ model/src/main/java/se/inera/certificate/service/CertificateService.java
@@ -0,0 +1,16 @@
+package se.inera.certificate.service;
+
+import se.inera.certificate.model.Certificate;
+import se.inera.certificate.model.CertificateMetaData;
+
+import java.util.List;
+
+/**
+ * @author andreaskaltenbach
+ */
+public interface CertificateService {
+
+    List<CertificateMetaData> listCertificates(String civicRegistrationNumber, List<String> type);
+
+    Certificate getCertificate(String civicRegistrationNumber, String id);
+}
---
+++ model/src/main/java/se/inera/certificate/service/impl/CertificateServiceImpl.java
@@ -0,0 +1,24 @@
+package se.inera.certificate.service.impl;
+
+import se.inera.certificate.model.Certificate;
+import se.inera.certificate.model.CertificateMetaData;
+import se.inera.certificate.service.CertificateService;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author andreaskaltenbach
+ */
+public class CertificateServiceImpl implements CertificateService {
+
+    @Override
+    public List<CertificateMetaData> listCertificates(String civicRegistrationNumber, List<String> type) {
+        return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public Certificate getCertificate(String civicRegistrationNumber, String id) {
+        return new Certificate();
+    }
+}
