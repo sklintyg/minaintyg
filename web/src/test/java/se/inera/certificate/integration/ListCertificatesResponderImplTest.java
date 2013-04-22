dd6c84cbe17888bd944288e419a3f042ebecb6ff
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-04-22 13:57:23 +0200

    Verify that type list aguments is actually used.

--- web/src/test/java/se/inera/certificate/integration/ListCertificatesResponderImplTest.java
+++ web/src/test/java/se/inera/certificate/integration/ListCertificatesResponderImplTest.java
@@ -1,26 +1,25 @@
 package se.inera.certificate.integration;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static se.inera.ifv.insuranceprocess.healthreporting.v2.ResultCodeEnum.OK;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.w3.wsaddressing10.AttributedURIType;
+
 import se.inera.certificate.model.CertificateMetaData;
 import se.inera.certificate.service.CertificateService;
 import se.inera.ifv.insuranceprocess.healthreporting.listcertificates.v1.rivtabp20.ListCertificatesResponderInterface;
 import se.inera.ifv.insuranceprocess.healthreporting.listcertificatesresponder.v1.ListCertificatesRequestType;
 import se.inera.ifv.insuranceprocess.healthreporting.listcertificatesresponder.v1.ListCertificatesResponseType;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static se.inera.ifv.insuranceprocess.healthreporting.v2.ResultCodeEnum.OK;
 
 /**
  * @author andreaskaltenbach
@@ -35,33 +34,30 @@ public class ListCertificatesResponderImplTest {
     private ListCertificatesResponderInterface responder = new ListCertificatesResponderImpl();
 
     @Test
-    public void listAnyCertificatesForUnknownPersonGivesEmptyList() {
-        AttributedURIType logicalAddress = null;
-        ListCertificatesRequestType parameters = new ListCertificatesRequestType();
-        parameters.setNationalIdentityNumber("unknown-person");
-        ListCertificatesResponseType result = responder.listCertificates(logicalAddress, parameters);
-        assertTrue(result.getMeta().isEmpty());
-    }
-
-    @Test
     public void listCertificatesWithNoCertificates() {
         String civicRegistrationNumber = "19350108-1234";
-        List<String> certificateTypes = Arrays.asList("fk7263");
+        List<String> certificateTypes = Collections.singletonList("fk7263");
 
         List<CertificateMetaData> result = Collections.emptyList();
+
         when(certificateService.listCertificates(civicRegistrationNumber, certificateTypes)).thenReturn(result);
 
-        ListCertificatesRequestType parameters = createListCertificatesRequest(civicRegistrationNumber);
+        ListCertificatesRequestType parameters = createListCertificatesRequest(civicRegistrationNumber, certificateTypes);
 
         ListCertificatesResponseType response = responder.listCertificates(null, parameters);
+
+        verify(certificateService).listCertificates(civicRegistrationNumber, certificateTypes);
 
         assertEquals(0, response.getMeta().size());
         assertEquals(OK, response.getResult().getResultCode());
     }
 
-    private ListCertificatesRequestType createListCertificatesRequest(String civicRegistrationNumber) {
+    private ListCertificatesRequestType createListCertificatesRequest(String civicRegistrationNumber, List<String> types) {
         ListCertificatesRequestType parameters = new ListCertificatesRequestType();
         parameters.setNationalIdentityNumber(civicRegistrationNumber);
+        for (String type: types) {
+            parameters.getCertificateType().add(type);
+        }
         return parameters;
     }
 }
