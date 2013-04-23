ac44dcf949c173d414a06abc86d6152d471193bd
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-04-23 12:08:19 +0200

    enabling to start server using dev profile together with H2 in-memory database

--- web/src/main/java/se/inera/certificate/service/impl/CertificateServiceImpl.java
+++ web/src/main/java/se/inera/certificate/service/impl/CertificateServiceImpl.java
@@ -1,11 +1,12 @@
 package se.inera.certificate.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.inera.certificate.dao.CertificateDao;
 import se.inera.certificate.model.Certificate;
 import se.inera.certificate.model.CertificateMetaData;
 import se.inera.certificate.service.CertificateService;
 
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,13 +15,16 @@ import java.util.List;
 @Service
 public class CertificateServiceImpl implements CertificateService {
 
+    @Autowired
+    private CertificateDao certificateDao;
+
     @Override
     public List<CertificateMetaData> listCertificates(String civicRegistrationNumber, List<String> type) {
-        return Collections.emptyList();
+        return certificateDao.findMetaDataByCivicRegistrationNumberAndType(civicRegistrationNumber, type);
     }
 
     @Override
     public Certificate getCertificate(String civicRegistrationNumber, String id) {
-        return new Certificate();
+        return certificateDao.getCertificate(id);
     }
 }
