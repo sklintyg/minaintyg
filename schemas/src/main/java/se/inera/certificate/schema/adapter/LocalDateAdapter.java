9cab47b921b373e97f961f15c95463272f959f0c
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-04-23 10:20:38 +0200

    Conversion of xs:dateTime to LocalDateTime for JAXB classes

--- schemas/src/main/java/se/inera/certificate/schema/adapter/LocalDateAdapter.java
+++ schemas/src/main/java/se/inera/certificate/schema/adapter/LocalDateAdapter.java
@@ -1,28 +1,55 @@
 package se.inera.certificate.schema.adapter;
 
 import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
 
 /**
- * Adapter for converting xs:date entities to LocalDate (Joda Time) and vice versa.
+ * Adapter for converting XML Schema types to Java dates and vice versa.
  *
  * @author andreaskaltenbach
  */
 public final class LocalDateAdapter {
 
-  private static final int TIME_ZONE_INDEX = 10;
+    private static final int DATE_END_INDEX = 10;
+    private static final int TIME_END_INDEX = 23;
 
-  private LocalDateAdapter() {}
-
-  public static LocalDate parseDate(String dateString) {
-    if (dateString.length() > TIME_ZONE_INDEX) {
-        return new LocalDate(dateString.substring(0, TIME_ZONE_INDEX));
+    private LocalDateAdapter() {
     }
-    else {
-        return new LocalDate(dateString);
-    }
-  }
 
-  public static String printDate(LocalDate date) {
-    return date.toString();
-  }
+
+    /**
+     *  Converts an xs:date to a Joda Time LocalDate.
+     */
+    public static LocalDate parseDate(String dateString) {
+        if (dateString.length() > DATE_END_INDEX) {
+            return new LocalDate(dateString.substring(0, DATE_END_INDEX));
+        } else {
+            return new LocalDate(dateString);
+        }
+    }
+
+    /**
+     *  Converts an xs:datetime to a Joda Time LocalDateTime.
+     */
+    public static LocalDateTime parseDateTime(String dateString) {
+        if (dateString.length() > TIME_END_INDEX) {
+            return new LocalDateTime(dateString.substring(0, TIME_END_INDEX));
+        } else {
+            return new LocalDateTime(dateString);
+        }
+    }
+
+    /**
+     * Converts a Joda Time LocalDateTime to an xs:datetime.
+     */
+    public static String printDateTime(LocalDateTime dateTime) {
+        return dateTime.toString();
+    }
+
+    /**
+     * Converts a Joda Time LocalDate to an xs:date.
+     */
+    public static String printDate(LocalDate date) {
+        return date.toString();
+    }
 }
--- schemas/src/test/java/se/inera/certificate/schema/adapter/LocalDateAdapterTest.java
+++ schemas/src/test/java/se/inera/certificate/schema/adapter/LocalDateAdapterTest.java
@@ -1,6 +1,7 @@
 package se.inera.certificate.schema.adapter;
 
 import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -15,28 +16,54 @@ public final class LocalDateAdapterTest {
     private static final String DATE_STRING = "2012-11-13";
 
     private static final LocalDate DATE = new LocalDate(2012, 11, 13);
+    private static final LocalDateTime DATE_TIME = new LocalDateTime(2012, 11, 13, 13, 55, 50, 844);
+    private static final LocalDateTime DATE_TIME_START_OF_DAY_STRING = new LocalDateTime(2012, 11, 13, 0, 0, 0);
 
     @Test
     public void testParseDateTimeWithTimeZoneInformation() {
+        LocalDateTime date = LocalDateAdapter.parseDateTime(DATE_TIME_STRING_WITH_TIME_ZONE);
+        assertEquals(DATE_TIME, date);
+    }
+
+    @Test
+    public void testParseDateWithTimeZoneInformation() {
         LocalDate date = LocalDateAdapter.parseDate(DATE_TIME_STRING_WITH_TIME_ZONE);
         assertEquals(DATE, date);
     }
 
     @Test
-    public void testParseDateTime() {
+    public void testParseDateWithDate() {
+        LocalDate date = LocalDateAdapter.parseDate(DATE_STRING);
+        assertEquals(DATE, date);
+    }
+
+    @Test
+    public void testParseDateTimeWithDate() {
+        LocalDateTime date = LocalDateAdapter.parseDateTime(DATE_STRING);
+        assertEquals(DATE_TIME_START_OF_DAY_STRING, date);
+    }
+
+    @Test
+    public void testParseDateWithDateTime() {
         LocalDate date = LocalDateAdapter.parseDate(DATE_TIME_STRING);
         assertEquals(DATE, date);
     }
 
     @Test
-    public void testParseDateWithoutTimeInformation() {
-        LocalDate date = LocalDateAdapter.parseDate(DATE_STRING);
-        assertEquals(DATE, date);
+    public void testParseDateTimeWithDateTime() {
+        LocalDateTime date = LocalDateAdapter.parseDateTime(DATE_TIME_STRING);
+        assertEquals(DATE_TIME, date);
     }
 
     @Test
     public void testPrintDate() {
         String dateString = LocalDateAdapter.printDate(DATE);
         assertEquals(DATE_STRING, dateString);
+    }
+
+    @Test
+    public void testPrintDateTime() {
+        String dateString = LocalDateAdapter.printDateTime(DATE_TIME);
+        assertEquals(DATE_TIME_STRING, dateString);
     }
 }
