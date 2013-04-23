a1d5bd56189b139f057ad5b710b2d2018ed6e108
Pär Wenåker <par.wenaker@callistaenterprise.se>
2013-04-23 13:51:17 +0200

    Changed return to MetaData

--- model/src/main/java/se/inera/certificate/dao/CertificateDao.java
+++ model/src/main/java/se/inera/certificate/dao/CertificateDao.java
@@ -2,14 +2,13 @@ package se.inera.certificate.dao;
 
 import java.util.List;
 
-import se.inera.certificate.model.Certificate;
 import se.inera.certificate.model.CertificateMetaData;
 
 public interface CertificateDao {
 
     List<CertificateMetaData> findMetaDataByCivicRegistrationNumberAndType(String civicRegistrationNumber, List<String> type);
 
-    Certificate getCertificate(String certificateId);
+    CertificateMetaData getCertificate(String certificateId);
     
     void store(CertificateMetaData certificateMetaData);
 }
