74add003e64829316851954ffec9f4aee7a97c9a
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-04-24 09:55:23 +0200

    Added service annotation so that autowire works...

--- web/src/main/java/se/inera/certificate/web/service/CitizenService.java
+++ web/src/main/java/se/inera/certificate/web/service/CitizenService.java
@@ -1,9 +1,11 @@
 package se.inera.certificate.web.service;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import se.inera.certificate.web.security.Citizen;
 
+@Service
 public class CitizenService {
 
 	public Citizen getCitizen() {
