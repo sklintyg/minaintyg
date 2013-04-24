7493e6616b1caca95715306dfd9aee187343de4e
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-04-24 16:07:29 +0200

    Util for creating ResultOfCall for failing web service executions

--- web/src/main/java/se/inera/certificate/integration/ResultOfCallUtil.java
+++ web/src/main/java/se/inera/certificate/integration/ResultOfCallUtil.java
@@ -1,5 +1,6 @@
 package se.inera.certificate.integration;
 
+import se.inera.ifv.insuranceprocess.healthreporting.v2.ErrorIdEnum;
 import se.inera.ifv.insuranceprocess.healthreporting.v2.ResultCodeEnum;
 import se.inera.ifv.insuranceprocess.healthreporting.v2.ResultOfCall;
 
@@ -13,6 +14,14 @@ public final class ResultOfCallUtil {
     public static ResultOfCall okResult() {
         ResultOfCall result = new ResultOfCall();
         result.setResultCode(ResultCodeEnum.OK);
+        return result;
+    }
+
+    public static ResultOfCall failResult(String errorText) {
+        ResultOfCall result = new ResultOfCall();
+        result.setResultCode(ResultCodeEnum.ERROR);
+        result.setErrorId(ErrorIdEnum.VALIDATION_ERROR);
+        result.setErrorText(errorText);
         return result;
     }
 }
