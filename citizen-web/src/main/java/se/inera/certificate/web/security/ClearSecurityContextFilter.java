a1c3d717a810d8c24430e8460d087ca7b6188af3
Johannes Carlén <johannes.carlen@callistaenterprise.se>
2013-05-02 07:27:43 +0200

    Clear security context before preauthentication

---
+++ citizen-web/src/main/java/se/inera/certificate/web/security/ClearSecurityContextFilter.java
@@ -0,0 +1,39 @@
+package se.inera.certificate.web.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class ClearSecurityContextFilter extends GenericFilterBean {
+
+    private static final Logger log = LoggerFactory.getLogger(ClearSecurityContextFilter.class);
+
+    LogoutHandler handler;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        log.debug("Clearing security context");
+        handler.logout((HttpServletRequest)request, (HttpServletResponse)response, null);
+        filterChain.doFilter(request, response);
+
+    }
+
+    public void setHandler(LogoutHandler handler) {
+        this.handler = handler;
+    }
+
+}
