1d8b65ff84d2a95bd3bec52b6bdaf15c909d0d9f
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-05-05 22:08:20 +0200

    First attempt at using angular ng-show/hide to replace jquery table pagination

--- citizen-web/src/main/webapp/js/list/controllers.js
+++ citizen-web/src/main/webapp/js/list/controllers.js
@@ -2,13 +2,18 @@
 
 /* Controllers */
 listCertApp.controller('ListCtrl', [ '$scope', 'listCertService', function ListCertCtrl($scope, listCertService) {
-    listCertService.getCertificates(function (list) {
+    $scope.certificates = [];
+
+    $scope.initialDisplaySize = 10;
+    $scope.currentDisplaySize = 10;
+
+    listCertService.getCertificates(function(list) {
         $scope.certificates = list;
     });
 } ]);
 
 listCertApp.controller('ListArchivedCtrl', [ '$scope', 'listCertService', function ListCertCtrl($scope, listCertService) {
-    listCertService.getCertificates(function (list) {
+    listCertService.getCertificates(function(list) {
         $scope.certificates = list;
     });
 } ]);
