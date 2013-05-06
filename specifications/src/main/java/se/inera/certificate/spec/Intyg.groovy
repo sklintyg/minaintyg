a1f84594940ed69e4f9c06255134e816fb02162a
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-05-06 16:59:11 +0200

    Extending the Fitnesse certificate setup. Certificate uploads now also contain valid FK7263 documents attached matching the certificate meta data

--- specifications/src/main/java/se/inera/certificate/spec/Intyg.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/Intyg.groovy
@@ -1,5 +1,8 @@
 package se.inera.certificate.spec
+
+import groovy.xml.XmlUtil
 import groovyx.net.http.RESTClient
+import org.springframework.core.io.ClassPathResource
 import se.inera.certificate.spec.util.RestClientFixture
 
 import static groovyx.net.http.ContentType.JSON
@@ -31,8 +34,38 @@ public class Intyg extends RestClientFixture {
                 validFromDate:datum,
                 validToDate:datum,
          careUnitName: 'Betty Ford Center',
-         document: '<certificate></certificate>'
+         document: document()
         ]
+    }
+
+    private document() {
+        if (typ == 'FK7263') {
+            fk7263Document()
+        }
+        else {
+            '<certificate></certificate>'
+        }
+
+    }
+
+    private fk7263Document() {
+        // slurping the FK7263 template
+        def registerMedicalCertificate = new XmlSlurper().parse(new ClassPathResource("fk7263_template.xml").getFile())
+
+        // setting the certificate ID
+        registerMedicalCertificate.lakarutlatande.'lakarutlatande-id' = id
+
+        // setting personnr in certificate XML
+        registerMedicalCertificate.lakarutlatande.patient.'person-id'.setProperty('@extension', personnr)
+
+        // setting the signing date, from date and to date
+        registerMedicalCertificate.lakarutlatande.signeringsdatum = datum
+        registerMedicalCertificate.lakarutlatande.signeringsdatum = datum
+        registerMedicalCertificate.lakarutlatande.funktionstillstand.arbetsformaga.arbetsformagaNedsattning.varaktighetFrom = datum
+        registerMedicalCertificate.lakarutlatande.funktionstillstand.arbetsformaga.arbetsformagaNedsattning.varaktighetTom = datum
+
+
+        XmlUtil.serialize(registerMedicalCertificate);
     }
 
 }
