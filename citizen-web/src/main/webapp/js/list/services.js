efbbed97d2630e3d93aadc9aaa56e42d924fbd06
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-05-06 10:10:52 +0200

    added cache support in ListCertService

--- citizen-web/src/main/webapp/js/list/services.js
+++ citizen-web/src/main/webapp/js/list/services.js
@@ -3,17 +3,30 @@
 /* Services */
 
 /*
-This service gets a list of all certificates.
+ * This service gets a list of all certificates. It also caches the list to
+ * avoid having to fetch the list every time a controller requests it.
  */
-listCertApp.factory('listCertService', [ '$http', function ($http) {
-    return {
-        getCertificates: function (callback) {
-            $http.get('/api/certificates').success(function (data) {
-                console.log(data.certificateList[0].date);
-                callback(data.certificateList);
-            }).error(function (data, status, headers, config) {
-                    console.log("error");
-                });
+listCertApp.factory('listCertService', [ '$http', function($http) {
+
+    // cached certificates response
+    var cachedList = null;
+
+    function _getCertificates(callback) {
+        if (cachedList != null) {
+            console.log("returning cached response");
+            callback(cachedList);
+            return;
         }
+        $http.get('/api/certificates').success(function(data) {
+            console.log("populating cache");
+            cachedList = data.certificateList;
+            callback(cachedList);
+        }).error(function(data, status, headers, config) {
+            console.log("error");
+        });
+    }
+    // Return public API for our service
+    return {
+        getCertificates : _getCertificates
     }
 } ]);
