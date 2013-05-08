456515141ff4708aac4b8d17784d0ad009b33089
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-05-08 13:21:55 +0200

    new around advice which turns exceptions into web service responses containing a ResultOfCall element

---
+++ web/src/main/java/se/inera/certificate/integration/aop/ExceptionTransformingAroundAdvice.java
@@ -0,0 +1,37 @@
+package se.inera.certificate.integration.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import se.inera.certificate.integration.ResultOfCallUtil;
+import se.inera.certificate.response.WebServiceResponse;
+
+/**
+ * AspectJ around advice applied around all web service methods.
+ *
+ * If the surrounded web service method call results in an exception, this advice handles the exception and converts it
+ * to a proper web service response containing a ResultOfCall with result code set to ERROR. The exception's message
+ * will be used as error text.
+ *
+ * @author andreaskaltenbach
+ */
+@Aspect
+public class ExceptionTransformingAroundAdvice {
+
+    @Around("execution(public se.inera.certificate.response.WebServiceResponse+ se.inera.certificate.integration.*.*(..))")
+    public Object doRecoveryActions(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            if (joinPoint.getSignature() instanceof MethodSignature) {
+                Class<WebServiceResponse> returnType = ((MethodSignature) joinPoint.getSignature()).getReturnType();
+                WebServiceResponse webServiceResponse = returnType.newInstance();
+                webServiceResponse.setResult(ResultOfCallUtil.applicationErrorResult(throwable.getMessage()));
+                return webServiceResponse;
+            }
+            throw throwable;
+        }
+
+    }
+}
