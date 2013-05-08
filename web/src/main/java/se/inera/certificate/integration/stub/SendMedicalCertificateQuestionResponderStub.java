d8fedbbd2a331c51d8b1e4b827954672c7f86a42
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-05-08 12:35:20 +0200

    Cleanup code idioms.

--- web/src/main/java/se/inera/certificate/integration/stub/SendMedicalCertificateQuestionResponderStub.java
+++ web/src/main/java/se/inera/certificate/integration/stub/SendMedicalCertificateQuestionResponderStub.java
@@ -4,9 +4,6 @@ import static se.inera.certificate.integration.ResultOfCallUtil.failResult;
 
 import java.io.StringWriter;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
@@ -19,9 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.w3.wsaddressing10.AttributedURIType;
 
 import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestion.v1.rivtabp20.SendMedicalCertificateQuestionResponderInterface;
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestionresponder.v1.ObjectFactory;
 import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestionresponder.v1.SendMedicalCertificateQuestionResponseType;
 import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestionresponder.v1.SendMedicalCertificateQuestionType;
-import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestionresponder.v1.ObjectFactory;
 
 /**
  * @author par.wenaker
@@ -30,17 +27,15 @@ import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequest
 @SchemaValidation
 public class SendMedicalCertificateQuestionResponderStub implements SendMedicalCertificateQuestionResponderInterface {
 
-    Logger logger = LoggerFactory.getLogger(SendMedicalCertificateQuestionResponderStub.class);
+    private Logger logger = LoggerFactory.getLogger(SendMedicalCertificateQuestionResponderStub.class);
 
     @Override
     public SendMedicalCertificateQuestionResponseType sendMedicalCertificateQuestion(AttributedURIType logicalAddress, SendMedicalCertificateQuestionType request) {
         
         SendMedicalCertificateQuestionResponseType response = new SendMedicalCertificateQuestionResponseType();
         
-        // unmarshal the certificate document
-        String document;
         try {
-            document = marshalCertificate(request);
+            marshalCertificate(request);
             logger.info("STUB Received request");
         } catch (JAXBException e) {
             response.setResult(failResult("Unable to marshal certificate information"));
