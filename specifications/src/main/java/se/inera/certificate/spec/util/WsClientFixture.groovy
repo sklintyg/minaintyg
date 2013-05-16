4de3b9b1f2f32c77e73a2fbea12df53b5a817211
Bjorn Beskow <bb@callistaenterprise.se>
2013-05-16 10:16:34 +0200

    Get baseUrl from system property, if present

--- specifications/src/main/java/se/inera/certificate/spec/util/WsClientFixture.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/util/WsClientFixture.groovy
@@ -7,11 +7,11 @@ import org.w3.wsaddressing10.AttributedURIType
 
 class WsClientFixture {
 
-	static String baseUrl = "http://localhost:8080/inera-certificate"
+	static String baseUrl = System.getProperty("certificate.baseUrl","http://localhost:8080/inera-certificate/")
 	AttributedURIType logicalAddress = new AttributedURIType()
 	
 	def setEndpoint(def responder, String serviceName) {
 	Client client = ClientProxy.getClient(responder);
-		client.getRequestContext().put(Message.ENDPOINT_ADDRESS, baseUrl + "/" +serviceName) ;
+		client.getRequestContext().put(Message.ENDPOINT_ADDRESS, baseUrl + serviceName) ;
 	}
 }
