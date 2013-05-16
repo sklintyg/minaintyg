2f98c8c25951ef5e6254064251f3afb34b0dbc0a
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-05-16 16:10:17 +0200

    removed obsolete controller mapping

--- citizen-web/src/main/java/se/inera/certificate/web/controller/PageController.java
+++ citizen-web/src/main/java/se/inera/certificate/web/controller/PageController.java
@@ -42,7 +42,7 @@ public class PageController {
     @Autowired
     private CitizenService citizenService;
 
-    @RequestMapping(value = { "/sso", "/fakesso" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/sso" }, method = RequestMethod.GET)
     public String sso() {
         LOG.debug("sso");
         Citizen citizen = citizenService.getCitizen();
