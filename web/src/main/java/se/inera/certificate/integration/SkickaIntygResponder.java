92b40f04efa828ff638637c729d6f9cc0ad66ea7
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-05-23 09:08:39 +0200

    making lakarutlatande root element in generic certificate XSD, allowing ANY elements as children

--- web/src/main/java/se/inera/certificate/integration/SkickaIntygResponder.java
+++ web/src/main/java/se/inera/certificate/integration/SkickaIntygResponder.java
@@ -1,17 +1,31 @@
 package se.inera.certificate.integration;
 
 import intyg.skickaintyg._1.SkickaIntygResponderInterface;
-import se.inera.certificate.integration.v1.IntygType;
+import org.apache.cxf.annotations.SchemaValidation;
+import se.inera.certificate.integration.v1.LakarutlatandeType;
 
 import javax.xml.ws.Holder;
 
 /**
  * @author andreaskaltenbach
  */
+@SchemaValidation
 public class SkickaIntygResponder implements SkickaIntygResponderInterface {
 
     @Override
-    public void skickaIntyg(Holder<IntygType> intyg) {
-        intyg.value.getLakarutlatande().setId("hihiDe");
+    public void skickaIntyg(Holder<LakarutlatandeType> intyg) {
+        intyg.value.setId("hihiDe");
+
+        // validate certificate
+
+        // if invalid -> return validation errors
+
+        // if valid -> proceed with storing the certificate along with its custom binary info
+
+
+    }
+
+    private void validate() {
+        // send certificate to revalidate certificate agaist
     }
 }
