c8c4bc104ede57239d425222984e6804c26b1113
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-05-24 11:09:28 +0200

    extended mocked LakarutlatandeResource to respond with rli certificate model if id is prefixed with rli, otherwise an fk7263 is returned

--- web/src/main/java/se/inera/certificate/integration/rest/LakarutlatandeResource.java
+++ web/src/main/java/se/inera/certificate/integration/rest/LakarutlatandeResource.java
@@ -1,11 +1,17 @@
 package se.inera.certificate.integration.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.joda.time.LocalDateTime;
+
 import se.inera.certificate.integration.IneraCertificateRestApi;
+import se.inera.certificate.model.HosPersonal;
 import se.inera.certificate.model.Lakarutlatande;
 import se.inera.certificate.model.Ovrigt;
 import se.inera.certificate.model.Patient;
+import se.inera.certificate.model.Vardenhet;
+import se.inera.certificate.model.Vardgivare;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author andreaskaltenbach
@@ -23,20 +29,65 @@ public class LakarutlatandeResource implements IneraCertificateRestApi {
     public String getCertificate(String certificateId) {
 
         try {
-            return OBJECT_MAPPER.writeValueAsString(lakarutlatande());
+            if (certificateId.startsWith("rli")) {
+                return OBJECT_MAPPER.writeValueAsString(lakarutlatande(certificateId));
+            } else {
+                return OBJECT_MAPPER.writeValueAsString(lakarutlatandeFk7263(certificateId));
+            }
+
         } catch (JsonProcessingException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace(); // To change body of catch statement use File | Settings | File Templates.
         }
         return "{\"failed\":true}";
     }
 
-    private Lakarutlatande lakarutlatande() {
+    private Lakarutlatande lakarutlatandeFk7263(String id) {
         Lakarutlatande lakarutlatande = new Lakarutlatande();
-        lakarutlatande.setId("123");
+        lakarutlatande.setId(id);
 
         Patient patient = new Patient();
         patient.setId("19001122-3344");
         lakarutlatande.setPatient(patient);
+        lakarutlatande.setKommentar("Lite svag i knäna");
+        lakarutlatande.setTyp("fk7263");
+        lakarutlatande.setSigneringsdatum(new LocalDateTime());
+        HosPersonal hosPersonal = new HosPersonal();
+        hosPersonal.setId("HOSID");
+
+        lakarutlatande.setSkapadAv(hosPersonal);
+        Vardenhet vardenhet = new Vardenhet();
+        vardenhet.setId("vardenhets-id");
+        vardenhet.setNamn("Vårdcentrum i väst");
+        Vardgivare vardgivare = new Vardgivare();
+        vardgivare.setId("vardgivar-id");
+        vardgivare.setNamn("Vård i Väst");
+        vardenhet.setVardgivare(vardgivare);
+        lakarutlatande.setVardenhet(vardenhet);
+        return lakarutlatande;
+    }
+
+    private Lakarutlatande lakarutlatande(String id) {
+        Lakarutlatande lakarutlatande = new Lakarutlatande();
+
+        lakarutlatande.setId(id);
+        lakarutlatande.setTyp("rli");
+        Patient patient = new Patient();
+        patient.setId("19001122-3344");
+        lakarutlatande.setPatient(patient);
+        lakarutlatande.setKommentar("Flygrädd som bara den!");
+        lakarutlatande.setSigneringsdatum(new LocalDateTime());
+        HosPersonal hosPersonal = new HosPersonal();
+        hosPersonal.setId("HOSID");
+
+        lakarutlatande.setSkapadAv(hosPersonal);
+        Vardenhet vardenhet = new Vardenhet();
+        vardenhet.setId("vardenhets-id");
+        vardenhet.setNamn("Flygrädslecentrum i väst");
+        Vardgivare vardgivare = new Vardgivare();
+        vardgivare.setId("vardgivar-id");
+        vardgivare.setNamn("Min privata klinik");
+        vardenhet.setVardgivare(vardgivare);
+        lakarutlatande.setVardenhet(vardenhet);
 
         Ovrigt ovrigt = new Ovrigt();
         ovrigt.setData("{\"resmal\":\"San Francisco\"}");
