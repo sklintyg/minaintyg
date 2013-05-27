72bf20e1ffb98528d920427976bd533e4c3ee5de
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-05-27 15:57:27 +0200

    Added test for sending certifictae to FK.

---
+++ specifications/src/main/java/se/inera/certificate/spec/SkickaIntygTillFk.groovy
@@ -0,0 +1,64 @@
+package se.inera.certificate.spec
+
+import iso.v21090.dt.v1.II
+
+import org.joda.time.LocalDateTime
+
+import riv.insuranceprocess.healthreporting.medcertqa._1.LakarutlatandeEnkelType
+import riv.insuranceprocess.healthreporting.medcertqa._1.VardAdresseringsType
+import se.inera.certificate.spec.util.WsClientFixture
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificate.v1.rivtabp20.SendMedicalCertificateResponderInterface
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificate.v1.rivtabp20.SendMedicalCertificateResponderService
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateresponder.v1.SendMedicalCertificateRequestType
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateresponder.v1.SendMedicalCertificateResponseType
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateresponder.v1.SendType
+import se.inera.ifv.insuranceprocess.healthreporting.v2.EnhetType
+import se.inera.ifv.insuranceprocess.healthreporting.v2.HosPersonalType
+import se.inera.ifv.insuranceprocess.healthreporting.v2.PatientType
+import se.inera.ifv.insuranceprocess.healthreporting.v2.VardgivareType
+
+/**
+ *
+ * @author andreaskaltenbach
+ */
+class SkickaIntygTillFk extends WsClientFixture {
+
+    private SendMedicalCertificateResponderService sendService = new SendMedicalCertificateResponderService();
+    private SendMedicalCertificateResponderInterface sendResponder = sendService.sendMedicalCertificateResponderPort
+
+    String personnummer
+    String intyg
+
+    public SkickaIntygTillFk() {
+        setEndpoint(sendResponder, "send-certificate/v1.0")
+    }
+
+    public void execute() {
+        SendMedicalCertificateRequestType sendRequestType = new SendMedicalCertificateRequestType()
+        SendType sendType = new SendType()
+        sendRequestType.setSend(sendType)
+        sendType.vardReferensId = 1
+        sendType.avsantTidpunkt = new LocalDateTime("2013-05-01T11:00:00")
+        sendType.adressVard = new VardAdresseringsType()        
+        sendType.adressVard.hosPersonal = new HosPersonalType()
+        sendType.adressVard.hosPersonal.personalId = new II()
+        sendType.adressVard.hosPersonal.personalId.extension = "personalid"
+        sendType.adressVard.hosPersonal.enhet = new EnhetType()
+        sendType.adressVard.hosPersonal.enhet.enhetsId = new II()
+        sendType.adressVard.hosPersonal.enhet.enhetsId.extension = "1"
+        sendType.adressVard.hosPersonal.enhet.enhetsnamn = "Enhetsnamn"
+        sendType.adressVard.hosPersonal.enhet.vardgivare = new VardgivareType()
+        sendType.adressVard.hosPersonal.enhet.vardgivare.vardgivareId = new II()
+        sendType.adressVard.hosPersonal.enhet.vardgivare.vardgivareId.extension = "1"
+        sendType.adressVard.hosPersonal.enhet.vardgivare.vardgivarnamn = "Vårdgivarnamn"
+        
+        sendType.lakarutlatande = new LakarutlatandeEnkelType()
+        sendType.lakarutlatande.lakarutlatandeId = intyg
+        sendType.lakarutlatande.signeringsTidpunkt = new LocalDateTime("2013-05-01T11:00:00")
+        sendType.lakarutlatande.patient = new PatientType()
+        sendType.lakarutlatande.patient.personId = new II()
+        sendType.lakarutlatande.patient.personId.extension = personnummer
+        
+        SendMedicalCertificateResponseType response = sendResponder.sendMedicalCertificate(null, sendRequestType)
+    }
+}
