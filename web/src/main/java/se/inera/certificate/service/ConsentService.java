7ed47e0af0d112d8acf1b9cc900cfd149593e9ba
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-06-03 14:38:07 +0200

    Made LakarutlatandeResource fetch data from persistent store.

--- web/src/main/java/se/inera/certificate/service/ConsentService.java
+++ web/src/main/java/se/inera/certificate/service/ConsentService.java
@@ -2,7 +2,19 @@ package se.inera.certificate.service;
 
 public interface ConsentService {
 
+    /**
+     * Consent status for given civicRegistrationNumber.
+     * 
+     * @param civicRegistrationNumber
+     * @return true if consent is registered for or civicRegistrationNumer is null 
+     */
     boolean isConsent(String civicRegistrationNumber);
 
+    /**
+     * Set consent status for given cuvucRegistrationNumber. True means consent is given and false means no consent is given.
+     * 
+     * @param civicRegistrationNumber
+     * @param consentGiven
+     */
     void setConsent(String civicRegistrationNumber, boolean consentGiven);
 }
