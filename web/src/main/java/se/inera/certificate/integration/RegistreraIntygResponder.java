e9deb558da410060ec7a3f8881a324562382fb8a
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-06-05 10:03:43 +0200

    validation module contract now uses HTTP status codes to indicate validity of a certificate. For invalid documents, HTTP 400 (Bad Request) is returned. This aligns with the behaviour of XSD schema validators.

--- web/src/main/java/se/inera/certificate/integration/RegistreraIntygResponder.java
+++ web/src/main/java/se/inera/certificate/integration/RegistreraIntygResponder.java
@@ -1,32 +1,36 @@
 package se.inera.certificate.integration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import intyg.registreraintyg._1.RegistreraIntygResponderInterface;
-
-import java.util.Collections;
-
-import javax.xml.ws.Holder;
-
+import org.apache.commons.io.IOUtils;
 import org.apache.cxf.annotations.SchemaValidation;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import se.inera.certificate.integration.converter.LakarutlatandeJaxbToLakarutlatandeConverter;
 import se.inera.certificate.integration.rest.ModuleRestApi;
 import se.inera.certificate.integration.v1.Lakarutlatande;
 import se.inera.certificate.integration.validator.ValidationException;
 import se.inera.certificate.model.Ovrigt;
-import se.inera.certificate.model.Valideringsresultat;
 import se.inera.certificate.service.CertificateService;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.xml.ws.Holder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
 
 /**
  * @author andreaskaltenbach
  */
 @SchemaValidation
 public class RegistreraIntygResponder implements RegistreraIntygResponderInterface {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistreraIntygResponder.class);
 
     private String host;
 
@@ -52,11 +56,11 @@ public class RegistreraIntygResponder implements RegistreraIntygResponderInterfa
 
         String certificateExtension = extractCertificateExtensionData(type, lakarutlatande.value);
 
-        se.inera.certificate.model.Lakarutlatande model  = LakarutlatandeJaxbToLakarutlatandeConverter.convert(lakarutlatande.value);
+        se.inera.certificate.model.Lakarutlatande model = LakarutlatandeJaxbToLakarutlatandeConverter.convert(lakarutlatande.value);
 
         Ovrigt ovrigt = new Ovrigt();
         ovrigt.setData(certificateExtension);
-        model.setOvrigt(ovrigt );
+        model.setOvrigt(ovrigt);
 
         certificateService.storeCertificate(model);
     }
@@ -71,23 +75,45 @@ public class RegistreraIntygResponder implements RegistreraIntygResponderInterfa
 
         ModuleRestApi endpoint = getModuleRestService(type);
 
-        Valideringsresultat result = endpoint.validate(lakarutlatande);
+        Response response = endpoint.validate(lakarutlatande);
 
-        if (!result.getFel().isEmpty()) {
-            throw new ValidationException(result.getFel());
+        switch (response.getStatus()) {
+            case 400: // BadRequest -> extract validation error message and transform to ValidationException
+                InputStream inputStream = (InputStream) response.getEntity();
+
+                try {
+                    String validationErrorMessage = IOUtils.toString(inputStream);
+                    throw new ValidationException(validationErrorMessage);
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to read response for validation of '" + type + "' certificate.");
+                }
+
+            case 200:
+                break;
+            default:
+                String errorMessage = "Failed to validate certificate for certificate type '" + type + "'. HTTP status code is " + response.getStatus();
+                LOGGER.error(errorMessage);
+                throw new RuntimeException(errorMessage);
         }
     }
 
     private String extractCertificateExtensionData(String type, Lakarutlatande lakarutlatande) {
 
+
         ModuleRestApi endpoint = getModuleRestService(type);
 
-        Object certificateExtension = endpoint.extract(lakarutlatande);
-
         try {
-            return objectMapper.writeValueAsString(certificateExtension);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to process JSON: " + certificateExtension, e);
+            Object certificateExtension = endpoint.extract(lakarutlatande);
+
+            try {
+                return objectMapper.writeValueAsString(certificateExtension);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Failed to process JSON: " + certificateExtension, e);
+            }
+
+        } catch (NotFoundException e) {
+            LOGGER.error("No certificate module available to extract specifics for certificate type " + type);
+            throw new RuntimeException("No certificate module available to extract specifics for certificate type " + type, e);
         }
     }
 }
--- web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApi.java
+++ web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApi.java
@@ -2,13 +2,13 @@ package se.inera.certificate.integration.rest;
 
 
 import se.inera.certificate.integration.v1.Lakarutlatande;
-import se.inera.certificate.model.Valideringsresultat;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author andreaskaltenbach
@@ -21,9 +21,11 @@ public interface ModuleRestApi {
     @Produces(MediaType.APPLICATION_JSON)
     Object extract(Lakarutlatande lakarutlatande);
 
+
     @POST
-    @Path("/valid")
-    @Consumes(MediaType.APPLICATION_XML)
-    @Produces(MediaType.APPLICATION_JSON)
-    Valideringsresultat validate(Lakarutlatande lakarutlatande);
+    @Path( "/valid" )
+    @Consumes( MediaType.APPLICATION_XML )
+    @Produces( MediaType.TEXT_PLAIN )
+    public Response validate(Lakarutlatande intyg);
+
 }
