e6e36acbc48a94f6d3656d285609419f5181980b
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-06-10 12:49:37 +0200

    Remove Comopnent annotion. We do not want this to be created automatically.

--- web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApiFactory.java
+++ web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApiFactory.java
@@ -2,17 +2,16 @@ package se.inera.certificate.integration.rest;
 
 import java.util.Collections;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 /**
  *  Factory which creates {@link ModuleRestApi} instances to communicate with the Rest API of the certificate modules.
  *
  * @author andreaskaltenbach
  */
-@Component
 public class ModuleRestApiFactory {
 
     private String host;
@@ -28,7 +27,8 @@ public class ModuleRestApiFactory {
      * Creates a {@link ModuleRestApi} for the given certificate type.
      */
     public ModuleRestApi getModuleRestService(String type) {
-        ModuleRestApi endpoint = JAXRSClientFactory.create(host + "/" + type + "/api", ModuleRestApi.class, Collections.singletonList(jacksonJsonProvider));
+        String uri = host + "/" + type + "/api";
+        ModuleRestApi endpoint = JAXRSClientFactory.create(uri, ModuleRestApi.class, Collections.singletonList(jacksonJsonProvider));
         return endpoint;
     }
 }
