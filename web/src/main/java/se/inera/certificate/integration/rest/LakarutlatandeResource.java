2fcfc62b0a2530d30d4d97e1ee3655ee6ddfdfbe
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-06-12 15:01:44 +0200

    PDF filename contains personnummer and from/to date

--- web/src/main/java/se/inera/certificate/integration/rest/LakarutlatandeResource.java
+++ web/src/main/java/se/inera/certificate/integration/rest/LakarutlatandeResource.java
@@ -1,19 +1,25 @@
 package se.inera.certificate.integration.rest;
 
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import se.inera.certificate.integration.IneraCertificateRestApi;
 import se.inera.certificate.model.Certificate;
 import se.inera.certificate.model.Lakarutlatande;
 import se.inera.certificate.service.CertificateService;
 
-import javax.ws.rs.core.Response;
-import java.io.IOException;
-
 /**
  * @author andreaskaltenbach
  */
 public class LakarutlatandeResource implements IneraCertificateRestApi {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LakarutlatandeResource.class);
+    private static final String CONTENT_DISPOSITION = "Content-Disposition";
+    private static final String DATE_FORMAT = "yyyyMMdd";
 
     @Autowired
     private CertificateService certificateService;
@@ -47,10 +53,22 @@ public class LakarutlatandeResource implements IneraCertificateRestApi {
 
         try {
             Lakarutlatande lakarutlatande = objectMapper.readValue(certificate.getDocument(), Lakarutlatande.class);
-            byte[] pdf = moduleRestApi.pdf(lakarutlatande);
-            return Response.ok(pdf).build();
+            Response response = moduleRestApi.pdf(lakarutlatande);
+            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+              LOG.error("Failed to create PDF for certificate #" + certificateId + ". Certificate module returned status code " + response.getStatus());
+                return Response.status(response.getStatus()).build();
+            }
+            return Response.ok(response.getEntity()).header(CONTENT_DISPOSITION, "attachment; filename=" + pdfFileName(lakarutlatande)).build();
         } catch (IOException e) {
-            throw new RuntimeException("Failed to unmarshall lakarutlatande for certificate " + certificateId, e);
+            LOG.error("Failed to unmarshall lakarutlatande for certificate " + certificateId, e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    private String pdfFileName(Lakarutlatande lakarutlatande) {
+        return String.format("läkarutlatande_%s_%s-%s.pdf",
+                lakarutlatande.getPatient().getId(),
+                lakarutlatande.calculateValidFromDate().toString(DATE_FORMAT),
+                lakarutlatande.calculateValidToDate().toString(DATE_FORMAT));
     }
 }
--- web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApi.java
+++ web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApi.java
@@ -1,14 +1,14 @@
 package se.inera.certificate.integration.rest;
 
 
-import se.inera.certificate.integration.v1.Lakarutlatande;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import se.inera.certificate.integration.v1.Lakarutlatande;
 
 /**
  * @author andreaskaltenbach
@@ -32,5 +32,5 @@ public interface ModuleRestApi {
     @Path( "/pdf" )
     @Consumes( MediaType.APPLICATION_JSON )
     @Produces( "application/pdf" )
-    byte[] pdf(se.inera.certificate.model.Lakarutlatande intyg);
+    Response pdf(se.inera.certificate.model.Lakarutlatande intyg);
 }
