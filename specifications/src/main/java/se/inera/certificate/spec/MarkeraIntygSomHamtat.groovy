0dd013d6b22ad491204b2e1136a16973e6253439
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-06-18 17:38:56 +0200

    using WsFixture's resultAsString

--- specifications/src/main/java/se/inera/certificate/spec/MarkeraIntygSomHamtat.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/MarkeraIntygSomHamtat.groovy
@@ -1,5 +1,4 @@
 package se.inera.certificate.spec
-
 import org.joda.time.LocalDateTime
 import se.inera.certificate.spec.util.WsClientFixture
 import se.inera.ifv.insuranceprocess.certificate.v1.StatusType
@@ -7,8 +6,6 @@ import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatus.v1.riv
 import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatus.v1.rivtabp20.SetCertificateStatusResponderService
 import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatusresponder.v1.SetCertificateStatusRequestType
 import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatusresponder.v1.SetCertificateStatusResponseType
-import se.inera.ifv.insuranceprocess.healthreporting.v2.ResultCodeEnum
-
 /**
  *
  * @author andreaskaltenbach
@@ -38,10 +35,6 @@ class MarkeraIntygSomHamtat extends WsClientFixture {
 
         SetCertificateStatusResponseType response = setCertificateStatusResponder.setCertificateStatus(logicalAddress, parameters)
 
-        if (response.result.resultCode == ResultCodeEnum.OK) {
-            return "ok"
-        } else {
-            return response.result.errorText
-        }
+        resultAsString(response)
     }
 }
