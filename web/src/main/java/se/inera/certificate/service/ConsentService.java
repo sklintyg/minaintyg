d761b1b2301be1804baaff028ed4b6e17b98a434
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-06-25 11:18:06 +0200

    Remove trailing whitespaces.

--- web/src/main/java/se/inera/certificate/service/ConsentService.java
+++ web/src/main/java/se/inera/certificate/service/ConsentService.java
@@ -4,15 +4,15 @@ public interface ConsentService {
 
     /**
      * Consent status for given civicRegistrationNumber.
-     * 
+     *
      * @param civicRegistrationNumber
-     * @return true if consent is registered for or civicRegistrationNumer is null 
+     * @return true if consent is registered for or civicRegistrationNumer is null
      */
     boolean isConsent(String civicRegistrationNumber);
 
     /**
      * Set consent status for given cuvucRegistrationNumber. True means consent is given and false means no consent is given.
-     * 
+     *
      * @param civicRegistrationNumber
      * @param consentGiven
      */
