ebe4246aad7818631dbb61d7af8cd12eb3bc8202
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-07-03 17:34:14 +0200

    Remove dependency to spring-tx.

--- citizen-web/src/main/java/se/inera/certificate/web/security/MvkPreAuthenticationCallback.java
+++ citizen-web/src/main/java/se/inera/certificate/web/security/MvkPreAuthenticationCallback.java
@@ -24,10 +24,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class MvkPreAuthenticationCallback implements PreAuthenticationCallback {
 
     private static final Logger LOG = LoggerFactory.getLogger(MvkPreAuthenticationCallback.class);
