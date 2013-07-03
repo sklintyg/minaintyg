2100dc0145228da3380de87f8cbb3aab1b0923ec
Benny Samuelsson <benny.samuelsson@usify.se>
2013-07-03 15:45:11 +0200

    Added mi-field directive

---
+++ citizen-web/src/main/webapp/js/modules/mi-certificate-directive.js
@@ -0,0 +1,20 @@
+/** Directives only used in certificate modules */
+
+angular.module('directives.mi').directive("miField", ['$rootScope', function($rootScope) {
+    return {
+        restrict : "E",
+	    transclude : true,
+        replace : true,
+        scope : {
+          title: "@",
+	      filled: "="
+        },
+        template :
+			'<div class="body-row">'
+           +'   <h3 class="title" ng-class="{ \'unfilled\' : !filled}"><span message key="{{ title }}"></span> <span message key="view.label.blank" ng-hide="{{filled}}"></span></h3>'
+           +'   <span class="text" ng-show="{{filled}}">'
+           +'       <span ng-transclude></span>'
+           +'   </span>'
+           +'</div>'
+    }
+} ]);
