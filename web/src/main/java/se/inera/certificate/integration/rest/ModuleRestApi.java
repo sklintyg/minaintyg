feb75d424dce66405d0bea4e23eb459265952136
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-07-31 10:09:49 +0200

    extending module Rest API contract with converter signatures for converting between module-internal and module-external representations

--- web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApi.java
+++ web/src/main/java/se/inera/certificate/integration/rest/ModuleRestApi.java
@@ -2,6 +2,7 @@ package se.inera.certificate.integration.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -14,15 +15,45 @@ import se.inera.certificate.model.Utlatande;
  */
 public interface ModuleRestApi {
 
+    /**
+     * @param utlatande
+     * @return
+     */
     @POST
-    @Path("/valid")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_PLAIN)
+    @Path( "/valid" )
+    @Consumes( MediaType.APPLICATION_JSON )
+    @Produces( MediaType.TEXT_PLAIN )
     Response validate(Utlatande utlatande);
 
+    /**
+     * @param utlatande
+     * @return
+     */
     @POST
-    @Path("/pdf")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces("application/pdf")
+    @Path( "/pdf" )
+    @Consumes( MediaType.APPLICATION_JSON )
+    @Produces( "application/pdf" )
     Response pdf(Utlatande utlatande);
+
+    /**
+     * Convert from module-external format to module-internal format.
+     *
+     * @param utlatande
+     * @return
+     */
+    @PUT
+    @Path( "/internal" )
+    @Consumes( MediaType.APPLICATION_JSON )
+    Response convertExternalToInternal(Utlatande utlatande);
+
+    /**
+     * Convert from module-internal format to module-external format.
+     *
+     * @param utlatande
+     * @return
+     */
+    @PUT
+    @Path( "/external" )
+    @Consumes( MediaType.APPLICATION_JSON )
+    Response convertInternalToExternal(Object utlatande);
 }
