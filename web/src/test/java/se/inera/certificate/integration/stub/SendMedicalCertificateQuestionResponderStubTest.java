ae7ba90a788481b7f6b4b47eddf50e186ddfbd6b
Bjorn Beskow <bb@callistaenterprise.se>
2013-08-15 08:40:26 +0200

    Fixed failing unit test

--- web/src/test/java/se/inera/certificate/integration/stub/SendMedicalCertificateQuestionResponderStubTest.java
+++ web/src/test/java/se/inera/certificate/integration/stub/SendMedicalCertificateQuestionResponderStubTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.w3.wsaddressing10.AttributedURIType;
 
+import riv.insuranceprocess.healthreporting.medcertqa._1.Amnetyp;
 import riv.insuranceprocess.healthreporting.medcertqa._1.LakarutlatandeEnkelType;
 import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestionresponder.v1.QuestionToFkType;
 import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificatequestionresponder.v1.SendMedicalCertificateQuestionType;
@@ -27,6 +28,7 @@ public class SendMedicalCertificateQuestionResponderStubTest {
         AttributedURIType logicalAddress = new AttributedURIType();
         SendMedicalCertificateQuestionType request = new SendMedicalCertificateQuestionType();
         QuestionToFkType question = new QuestionToFkType();
+        question.setAmne(Amnetyp.MAKULERING_AV_LAKARINTYG);
         LakarutlatandeEnkelType lakarutlatande = new LakarutlatandeEnkelType();
         lakarutlatande.setLakarutlatandeId("id-1234567890");
         question.setLakarutlatande(lakarutlatande );
