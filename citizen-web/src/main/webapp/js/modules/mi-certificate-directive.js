26cab2b28327a8fa280eb0cdfbd9c44be4c008f3
Benny Samuelsson <benny.samuelsson@usify.se>
2013-08-15 16:27:26 +0200

    Made filled optional on mi-field, fixed spacing on not filled message.

--- citizen-web/src/main/webapp/js/modules/mi-certificate-directive.js
+++ citizen-web/src/main/webapp/js/modules/mi-certificate-directive.js
@@ -7,14 +7,29 @@ angular.module('directives.mi').directive("miField", ['$rootScope', function($ro
         replace : true,
         scope : {
           title: "@",
-	      filled: "="
+	      filled: "=?"
         },
         template :
 			'<div class="body-row">'
-           +'   <h3 class="title" ng-class="{ \'unfilled\' : !filled}"><span message key="{{ title }}"></span><span ng-hide="filled"><span message key="view.label.blank"></span></span></h3>'
+           +'   <h3 class="title" ng-class="{ \'unfilled\' : !filled}"><span message key="{{ title }}"></span> <span ng-hide="filled"><span message key="view.label.blank"></span></span></h3>'
            +'   <span class="text" ng-show="filled">'
            +'       <span ng-transclude></span>'
            +'   </span>'
            +'</div>'
+
+
+/*
+		//TODO: fix this to use default value for filled if it is omitted (it is already optional but default value is undefined =?)
+		compile: function(element,attrs)
+	    {
+		    var filled = attrs.filled || true;
+		    var htmlText = '<div class="body-row">'
+		               +'   <h3 class="title" ng-class="{ \'unfilled\' : !'+filled+'}"><span message key="{{ title }}"></span> <span ng-hide="'+filled+'"><span message key="view.label.blank"></span></span></h3>'
+		               +'   <span class="text" ng-show="'+filled+'">'
+		               +'       <span ng-transclude></span>'
+		               +'   </span>'
+		               +'</div>';
+		    element.replaceWith(htmlText);
+	    }*/
     }
 } ]);
