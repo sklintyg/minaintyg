056f8494351fe22813164fa6be220c3fbba40d0a
Roger Lindsjö <roger.lindsjo@callistaenterprise.se>
2013-08-27 19:59:59 +0200

    Added debug logging for user lookup in SAML module.

--- citizen-web/src/main/java/se/inera/certificate/web/security/saml/CitizenAuthenticationService.java
+++ citizen-web/src/main/java/se/inera/certificate/web/security/saml/CitizenAuthenticationService.java
@@ -1,5 +1,7 @@
 package se.inera.certificate.web.security.saml;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.saml.SAMLCredential;
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
@@ -11,9 +13,12 @@ import se.inera.certificate.web.security.LoginMethodEnum;
 @Service
 public class CitizenAuthenticationService implements SAMLUserDetailsService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CitizenAuthenticationService.class);
+
     @Override
     public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
         String userid = credential.getNameID().getValue();
+        LOG.debug("SAML user: " + userid);
         return new CitizenImpl(userid.substring(0, 8) + "-" + userid.substring(8), LoginMethodEnum.FK);
     }
 
