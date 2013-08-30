cd0e2b572775a2337b95821f7039f9df29a24671
Benny Samuelsson <benny.samuelsson@usify.se>
2013-08-30 15:26:21 +0200

    Updated error message and added last resort links to the error pages (cherry picked from commit ae4a441321f1bc5a07e4a569ca1375d3ea166248)
    Signed-off-by: Benny Samuelsson <benny.samuelsson@usify.se>

--- citizen-web/src/main/webapp/js/modules/common-message-resources.js
+++ citizen-web/src/main/webapp/js/modules/common-message-resources.js
@@ -36,12 +36,12 @@ var commonMessageResources = {
         
         "common.module.message.sendingcertificate" : "Skickar intyg...",
 
-        "error.pagetitle": "Ett problem har uppstått i tjänsten",
-        "error.couldnotloadcertlist" : "Kunde inte hämta intyg just nu.",
-        "error.couldnotarchivecert" : "Intyget kunde inte flyttas till arkivet.",
-        "error.couldnotrestorecert" : "Intyget kunde inte återställas till inkorgen.",
-        "error.couldnotrevokeconsent" : "Kunde inte återta samtycke.",
-        "error.couldnotgiveconsent" : "Det gick inte att spara samtycke.",
+        "error.pagetitle": "Tekniskt fel",
+        "error.couldnotloadcertlist" : "Intygen i Inkorgen kunde inte visas. Om felet kvarstår kan du kontakta <a href=\"/web/start#/hjalp\">support</a>. Om du inte kan komma åt intyget på Mina intyg just nu så kan du kontakta din läkare för att få en kopia.<br><br><a href=\"/web/start\">Gå till Inkorgen och försök igen</a>.",
+        "error.couldnotarchivecert" : "Intyget kunde inte flyttas från Inkorgen till Arkiverade intyg. Om felet kvarstår kan du kontakta <a href='/web/start#/hjalp'>support</a>.<br><br><a href='/web/start'>Gå till Inkorgen och försök igen</a>.",
+        "error.couldnotrestorecert" : "Intyget kunde inte flyttas från Arkiverade intyg till Inkorgen. Om felet kvarstår kan du kontakta <a href='/web/start#/hjalp'>support</a>.<br><br><a href='/web/start#/arkiverade'>Gå till Arkiverade intyg och försök igen</a>.",
+        "error.couldnotrevokeconsent" : "Det gick inte att återta ditt samtycke för tillfället. Prova att vänta några minuter och försök att återta ditt samtycke igen. Om felet kvarstår kan du kontakta <a href='/web/start#/hjalp'>support</a>.<br><br><a href='/web/start#/omminaintyg'>Gå till Om mina sidor och försök igen</a>",
+        "error.couldnotgiveconsent" : "Ditt samtycke kunde inte registreras för tillfället. Detta innebär att du inte kan börja använda tjänsten Mina intyg just nu. Prova att vänta några minuter och försök att ge ditt samtycke igen. Om felet kvarstår kan du kontakta <a href='/web/start#/hjalp'>support</a>.<br><br>Om du inte kan komma åt intyget på Mina intyg just nu så kan du kontakta din läkare för att få en kopia.<br><br><a href='/web/visa-ge-samtycke#/start'>Försök igen</a>",
     },
     "en" : {
         "label.inbox" : "The INBOX",
