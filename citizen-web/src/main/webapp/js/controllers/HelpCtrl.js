85bd6ef1f013facab166b171436578cbf27282f8
Johan Lindholm <johan.lindholm@usify.se>
2014-05-22 10:52:21 +0200

    Förberett hjälp och support med en faq. Ändrat markup för denna sidan likt Om mina intyg.

--- citizen-web/src/main/webapp/js/controllers/HelpCtrl.js
+++ citizen-web/src/main/webapp/js/controllers/HelpCtrl.js
@@ -3,5 +3,24 @@ define([], function() {
 
 	return [ '$scope', function($scope) {
 		$scope.pagefocus = true;
+		
+		// Hold left side navigation state
+		$scope.visibility = {
+			"helpdescription" : true,
+			"helpfaq" : false
+		}
+
+		// Hold focus state for sub pages
+		$scope.subpagefocus = {
+			"helpdescription" : false,
+			"helpfaq" : false
+		}
+
+		$scope.navigateTo = function(section) {
+			angular.forEach($scope.visibility, function(value, key) {
+				$scope.visibility[key] = (key == section) ? true : false;
+				$scope.subpagefocus[key] = (key == section) ? true : false;
+			});
+		}
 	} ];
 });
