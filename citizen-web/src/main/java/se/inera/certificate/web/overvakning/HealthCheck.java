892ff9f2ba62b50ce6c2120b82d721bf52f24053
Erik Forsgren <erik.forsgren@r2m.se>
2014-06-05 15:29:02 +0200

    INTYG-1159 Status-sida för övervakning av Intygstjänst och Mina Intyg
    - En minimal pingsida för Mina Intyg finns nu på /health-check/health-check.jsp

---
+++ citizen-web/src/main/java/se/inera/certificate/web/overvakning/HealthCheck.java
@@ -0,0 +1,46 @@
+package se.inera.certificate.web.overvakning;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HealthCheck {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheck.class);
+
+    public Status getPing() {
+        boolean ok;
+        StopWatch sp = new StopWatch();
+        sp.start();
+        try {
+            ok = true;
+        } catch (Exception e) {
+            ok = false;
+        }
+        sp.stop();
+        LOGGER.info("Ping in Mina Intyg got:{} response time {}", ok, sp.getTime());
+        return createStatus(ok, sp.getTime());
+    }
+
+    private Status createStatus(boolean ok, long time) {
+        return new Status(time, ok);
+    }
+
+    public static final class Status {
+        private final long measurement;
+        private final boolean ok;
+
+        private Status(long measurement, boolean ok) {
+            this.measurement = measurement;
+            this.ok = ok;
+        }
+
+        public boolean isOk() {
+            return ok;
+        }
+
+        public long getMeasurement() {
+            return measurement;
+        }
+    }
+}
