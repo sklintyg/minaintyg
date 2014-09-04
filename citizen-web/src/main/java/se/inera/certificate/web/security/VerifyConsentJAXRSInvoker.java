a952922fe6d5434be9cbc08dc3929f991224d797
Orjan Lundberg <orjan.lundberg@callistaenterprise.se>
2014-09-04 17:32:50 +0200

    INTYG-1170 Need to allow certain methods for
    getModulesMap","onbeforeunload","giveConsent" are needed
    during bootstrap and also when the user does not have consent
    or revokes consent

--- citizen-web/src/main/java/se/inera/certificate/web/security/VerifyConsentJAXRSInvoker.java
+++ citizen-web/src/main/java/se/inera/certificate/web/security/VerifyConsentJAXRSInvoker.java
@@ -1,12 +1,16 @@
 package se.inera.certificate.web.security;
 
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.ws.rs.core.Response;
 
+import com.google.common.collect.ImmutableSet;
 import org.apache.cxf.jaxrs.JAXRSInvoker;
+import org.apache.cxf.jaxrs.model.OperationResourceInfo;
 import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.MessageContentsList;
 
@@ -38,36 +42,53 @@ public class VerifyConsentJAXRSInvoker extends JAXRSInvoker {
 
     private AtomicLong consentCounter = new AtomicLong();
 
+    private static final ImmutableSet<String> ALLOWED_METHODS = ImmutableSet.of("getModulesMap","onbeforeunload","giveConsent");
+
+
     @Override
     public Object invoke(Exchange exchange, Object requestParams, Object resourceObject) {
         LOG.debug("invoke(...)");
 
+
         LOG.debug("Verifying citizen consent...");
         // Get Citizen instance from context
         Citizen citizen = citizenService.getCitizen();
-        if(citizen == null) {
+        if (citizen == null) {
             LOG.error("State of consent not known -citizen was null");
             return new MessageContentsList(Response.status(Response.Status.FORBIDDEN).build());
         }
 
-        LOG.debug("Login from {} consentIsKnown: {} ", citizen.getLoginMethod().toString(),citizen.consentIsKnown());
+        LOG.debug("Login from {} consentIsKnown: {} ", citizen.getLoginMethod().toString(), citizen.consentIsKnown());
 
         if (!citizen.consentIsKnown()) {
             LOG.debug("State of consent not known - fetching consent status...");
             boolean consentResult = consentService.fetchConsent(citizen.getUsername());
             LOG.debug("Consent result is {}", consentResult);
-            // set the consent result so that we don't have to fetch it next time around
+            // set the consent result so that we don't have to fetch it next tinime around
             citizen.setConsent(consentResult);
         }
 
         // Check consent state of citizen
         if (!citizen.hasConsent()) {
-            LOG.warn("User: {} does not have consent",citizen.getUsername());
+            LOG.warn("User: {} does not have consent", citizen.getUsername());
+            final String methodName;
+            if (exchange != null) {
+                OperationResourceInfo ori = exchange.get(OperationResourceInfo.class);
+                Method m = ori.getMethodToInvoke();
+                LOG.debug("Method called is {} ", m.getName());
+                methodName =  m.getName();
+            } else {
+                methodName =  "";
+            }
 
-            try {
-                return new MessageContentsList(Response.status(Response.Status.FORBIDDEN).contentLocation(new URI("\\web\\visa-ge-samtycke#\\consent")).build());
-            } catch (URISyntaxException e) {
-                return new MessageContentsList(Response.status(Response.Status.FORBIDDEN).build());
+            if (ALLOWED_METHODS.contains(methodName)) {
+                LOG.debug("Allowing method {}", methodName);
+            } else {
+                try {
+                    return new MessageContentsList(Response.status(Response.Status.FORBIDDEN).contentLocation(new URI("\\web\\visa-ge-samtycke#\\consent")).build());
+                } catch (URISyntaxException e) {
+                    return new MessageContentsList(Response.status(Response.Status.FORBIDDEN).build());
+                }
             }
         }
         // We have a consent, let the request continue processing
