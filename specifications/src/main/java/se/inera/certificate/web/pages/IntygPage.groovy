f86ecf3e0a278e2769c183099845b0a2633c75ca
Erik Forsgren <erik.forsgren@r2m.se>
2014-12-16 16:44:27 +0100

    WEBCERT-1209 Ta bort och anpassa återstående testdata

--- specifications/src/main/java/se/inera/certificate/web/pages/IntygPage.groovy
+++ specifications/src/main/java/se/inera/certificate/web/pages/IntygPage.groovy
@@ -15,14 +15,11 @@ class IntygPage extends AbstractPage {
         confirmArchiveBtn(required: false) { $("#archive-button") }
         diagnosKod1(required: false) { $("#diagnosKod1").text() }
         diagnosKlartext1(required: false) { $("#diagnosKlartext1").text() }
-        diagnosKod2(required: false) { $("#diagnosKod2").text() }
-        diagnosKlartext2(required: false) { $("#diagnosKlartext2").text() }
-        diagnosKod3(required: false) { $("#diagnosKod3").text() }
-        diagnosKlartext3(required: false) { $("#diagnosKlartext3").text() }
         falt6aArbetsformedlingen(required: false) { $("span", text: contains("Kontakt med Arbetsförmedlingen")).text() }
         falt6aForetagshalsovarden(required: false) { $("span", text: contains("Kontakt med företagshälsovården")).text() }
         falt6aOvrigt(required: false) { $("span", text: contains("Övrigt:")).text() }
         falt8bNedsatt25 { $("#nedsattMed25") }
+        falt13Kommentar(required: false) { $("#falt13-kommentar").text() }
     }
 
     def startSendFlow() {
