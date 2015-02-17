68ae91088821b34066612b77d6050b30eb5eaaee
Per Beijer <per.beijer@usify.se>
2015-02-17 13:55:00 +0100

    Setup med intygsdata för fitnessetest HttpSakerhet

--- specifications/src/main/java/se/inera/certificate/web/spec/HttpSakerhet.groovy
+++ specifications/src/main/java/se/inera/certificate/web/spec/HttpSakerhet.groovy
@@ -28,7 +28,7 @@ class HttpSakerhet  extends RestClientFixture {
     }
 
     boolean modulApiSvarSkaEjKunnaCachas() {
-        hamtaHeader("/moduleapi/certificate/intyg-1", "Cache-Control") == "no-cache, no-store, max-age=0, must-revalidate"
+        hamtaHeader("/moduleapi/certificate/fit-intyg-1", "Cache-Control") == "no-cache, no-store, max-age=0, must-revalidate"
     }
 
     boolean enSidaSkaInteKunnaSattasIFrame() {
