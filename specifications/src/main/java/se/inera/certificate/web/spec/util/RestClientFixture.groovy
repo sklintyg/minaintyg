bbc43cb4dd19434a2ab118b239e1acfa1008a075
Bjorn Beskow <bb@callistaenterprise.se>
2015-03-26 07:59:00 +0100

    Uppgraderat till http-client-0.7.1, som löser problem med ssl och self-signed-certificates. Flytta gemensam funktionalitet i RestClientFixture och WebServiceFixture till tools.
    (cherry picked from commit 6984f7ad7df1af5f6bbea76f25712f0f55b922da)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

--- specifications/src/main/java/se/inera/certificate/web/spec/util/RestClientFixture.groovy
+++ specifications/src/main/java/se/inera/certificate/web/spec/util/RestClientFixture.groovy
@@ -1,34 +1,13 @@
 package se.inera.certificate.web.spec.util
 
 import groovyx.net.http.RESTClient
-import org.apache.http.conn.scheme.Scheme
-import org.apache.http.conn.ssl.SSLSocketFactory
-import se.inera.certificate.spec.util.ssl.TestSSLSocketFactory
-
-import java.security.KeyStore
 
 /**
  *
  * @author andreaskaltenbach
  */
-class RestClientFixture {
+class RestClientFixture extends se.inera.certificate.spec.util.RestClientFixture {
 
-    static String baseUrl = System.getProperty("geb.build.baseUrl")
+    String baseUrl = System.getProperty("geb.build.baseUrl")
 
-    /**
-     * Creates a RestClient which accepts all server certificates
-     * @return
-     */
-    static def createRestClient() {
-        def restClient = new RESTClient(baseUrl)
-
-        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-        trustStore.load(null, null);
-        SSLSocketFactory sf = new TestSSLSocketFactory(trustStore);
-        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-
-        restClient.client.connectionManager.schemeRegistry.register(new Scheme("https", sf, 443))
-
-        restClient
-    }
 }
