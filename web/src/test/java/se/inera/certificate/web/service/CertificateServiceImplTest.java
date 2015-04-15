32e642c98d574d7bb4e2cc3265bc487418a1d64c
Bjorn Beskow <bb@callistaenterprise.se>
2015-04-15 10:03:53 +0200

    Enhetstest för INTYG-1424

--- web/src/test/java/se/inera/certificate/web/service/CertificateServiceImplTest.java
+++ web/src/test/java/se/inera/certificate/web/service/CertificateServiceImplTest.java
@@ -7,7 +7,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import javax.xml.namespace.QName;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.cxf.binding.soap.SoapFault;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.junit.Before;
@@ -185,6 +188,42 @@ public class CertificateServiceImplTest {
         assertEquals("sent", apiResponse.getResultCode());
 
         verify(sendServiceMock).sendCertificateForCitizen("FKORG", request);
+    }
+
+    @Test
+    public void testSendCertificateReturnsValidationError() {
+        /* Given */
+        SendCertificateForCitizenType request = new SendCertificateForCitizenType();
+        request.setPersonId("19121212-1212");
+        request.setUtlatandeId("1234567890");
+        request.setMottagareId("FK");
+
+        SendCertificateForCitizenResponseType response = new SendCertificateForCitizenResponseType();
+        response.setResult(ResultTypeUtil.errorResult(ErrorIdType.VALIDATION_ERROR, "validation error"));
+
+        /* When */
+        when(sendServiceMock.sendCertificateForCitizen(any(String.class), any(SendCertificateForCitizenType.class))).thenReturn(response);
+
+        /* Then */
+        ModuleAPIResponse apiResponse = service.sendCertificate("19121212-1212", "1234567890", "FK");
+
+        /* Verify */
+        assertEquals("error", apiResponse.getResultCode());
+    }
+
+    @Test(expected = SoapFault.class)
+    public void testSendCertificateThrowsSoapFault() {
+        /* Given */
+        SendCertificateForCitizenType request = new SendCertificateForCitizenType();
+        request.setPersonId("19121212-1212");
+        request.setUtlatandeId("1234567890");
+        request.setMottagareId("FK");
+
+        /* When */
+        when(sendServiceMock.sendCertificateForCitizen(any(String.class), any(SendCertificateForCitizenType.class))).thenThrow(new SoapFault("server error", new QName("")));
+
+        /* Then */
+        service.sendCertificate("19121212-1212", "1234567890", "FK");
     }
 
     private ClinicalProcessCertificateMetaTypeBuilder getClinicalProcessCertificateMetaTypeBuilder(
