05d307f5a0bfcff9b874a9657c3d7f380a1b949f
Per Beijer <per.beijer@usify.se>
2015-11-25 17:52:42 +0100

    INTYG-1463: Fixat url för templates

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
                 options: {
                     module: 'minaintyg',
                     url: function(url) {
-                        return url.replace('../', '/');
+                        return '/' + url.replace('../', '/');
                     }
                 }
             }
