6cd68c9280b03231d755633e7123cfa74647be6b
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-11-26 17:12:36 +0100

    WEBCERT-2166: Fixed package names after moving files in common/util/logging

--- web/src/main/java/se/inera/certificate/web/service/MonitoringLogServiceImpl.java
+++ web/src/main/java/se/inera/certificate/web/service/MonitoringLogServiceImpl.java
@@ -22,7 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import se.inera.certificate.logging.LogMarkers;
+import se.inera.intyg.common.util.logging.LogMarkers;
 import se.inera.intyg.common.support.modules.support.api.dto.Personnummer;
 
 @Service
