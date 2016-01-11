6dc537c6b99ffabeacb07016e65f1d697e90eb48
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2016-01-12 00:11:25 +0100

    DEVENV: Fixed checkstyle warnings

--- web/src/main/java/se/inera/intyg/minaintyg/web/web/controller/moduleapi/ModuleApiController.java
+++ web/src/main/java/se/inera/intyg/minaintyg/web/web/controller/moduleapi/ModuleApiController.java
@@ -22,7 +22,11 @@ import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 
 import java.io.IOException;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
