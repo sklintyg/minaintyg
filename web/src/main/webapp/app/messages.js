4f07771d48d00329cda2b7e1eeb7559befd7dcec
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2017-04-12 20:35:53 +0200

    INTYG-3815: Ny app-header.

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -73,7 +73,7 @@ angular.module('minaintyg').constant('minaintyg.messages', {
         'nav.label.archived': 'Arkiverade intyg',
         'nav.label.aboutminaintyg': 'Om Mina intyg',
         'nav.label.help': 'Hjälp och support',
-        'nav.label.loggedinas': 'Du är inloggad som',
+        'nav.label.loggedinas': 'Du är inloggad som:',
 
         /* INBOX */
 
