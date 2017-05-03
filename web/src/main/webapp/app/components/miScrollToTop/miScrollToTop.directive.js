c3905bc4b4bc4e5afafdd57c44e909ee49c916a2
Kebire Breitholtz <kebire.breitholtz@inera.se>
2017-05-03 16:28:47 +0200

    INTYG-3820: first draft of a scroll to top button

---
+++ web/src/main/webapp/app/components/miScrollToTop/miScrollToTop.directive.js
@@ -0,0 +1,36 @@
+angular.module('minaintyg').directive('miScrollToTop',
+    function ($rootScope, $location) {
+        'use strict';
+
+        return {
+            restrict: 'E',
+            controller: function ($scope, $window, $document) {
+
+                $scope.lowerHalf = false;
+
+                $document.on('scroll', function () {
+                    var scrollbarPercentage = getScrollbarPercentage();
+                    if (
+                        (scrollbarPercentage > 0.5 && !$scope.lowerHalf) ||
+                        (scrollbarPercentage <= 0.5 && $scope.lowerHalf)
+                    ) {
+                        $scope.$apply(function () {
+                            $scope.lowerHalf = !$scope.lowerHalf;
+                        });
+                    }
+                });
+
+                function getScrollbarPercentage() {
+                    var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
+                    var percentage = (wintop / (docheight - winheight));
+                    return percentage;
+                }
+
+                $scope.scrollToTop = function () {
+                    $window.scrollTo(0, 0);
+                }
+
+            },
+            templateUrl: '/app/components/miScrollToTop/miScrollToTop.directive.html'
+        };
+    });
