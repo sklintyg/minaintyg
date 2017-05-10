ba054ee01d05b1273f1606f7ee2efb9e535c7362
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2017-05-10 15:53:50 +0200

    DEVENV: städat lite, fixat jshints mm.

--- web/src/main/webapp/app/components/miScrollToTop/miScrollToTop.directive.js
+++ web/src/main/webapp/app/components/miScrollToTop/miScrollToTop.directive.js
@@ -1,47 +1,40 @@
-angular.module('minaintyg').directive('miScrollToTop',
-    function ($rootScope, $location) {
-        'use strict';
+angular.module('minaintyg').directive('miScrollToTop', function() {
+    'use strict';
 
-        return {
-            restrict: 'E',
-            controller: function ($scope, $window, $document) {
+    return {
+        restrict: 'E',
+        templateUrl: '/app/components/miScrollToTop/miScrollToTop.directive.html',
+        controller: function($scope, $window, $document) {
 
+            $scope.lowerHalf = false;
+
+            $scope.$on('$locationChangeStart', function(event) {
                 $scope.lowerHalf = false;
+            });
 
-                $scope.$on('$locationChangeStart', function (event) {
-                    $scope.lowerHalf = false;
-                });
+            $document.on('scroll', function() {
+                checkScroller();
+            });
 
-                $document.on('scroll', function () {
-                    checkScroller();
-                });
-
-                function checkScroller() {
-                    var windowHeight = window.innerHeight;
-                    var currentScroll = $(document).scrollTop();
-                    if (currentScroll > (windowHeight / 2) && !$scope.lowerHalf) {
-                        $scope.$apply(function () {
-                            $scope.lowerHalf = true;
-                        });
-                    }
-                    else if (currentScroll < (windowHeight / 2) && $scope.lowerHalf) {
-                        $scope.$apply(function () {
-                            $scope.lowerHalf = false;
-                        });
-                    }
+            function checkScroller() {
+                var windowHeight = window.innerHeight;
+                var currentScroll = $(document).scrollTop();
+                if (currentScroll > (windowHeight / 2) && !$scope.lowerHalf) {
+                    $scope.$apply(function() {
+                        $scope.lowerHalf = true;
+                    });
+                } else if (currentScroll < (windowHeight / 2) && $scope.lowerHalf) {
+                    $scope.$apply(function() {
+                        $scope.lowerHalf = false;
+                    });
                 }
+            }
 
-                function getScrollbarPercentage() {
-                    var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
-                    var percentage = (wintop / (docheight - winheight));
-                    return percentage;
-                }
+            $scope.scrollToTop = function() {
+                $window.scrollTo(0, 0);
+            };
 
-                $scope.scrollToTop = function () {
-                    $window.scrollTo(0, 0);
-                }
+        }
 
-            },
-            templateUrl: '/app/components/miScrollToTop/miScrollToTop.directive.html'
-        };
-    });
+    };
+});
