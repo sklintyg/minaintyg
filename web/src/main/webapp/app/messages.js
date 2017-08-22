ff0dd131f0143bff0bd752ee77f583fd9ea4b88d
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2017-08-22 12:36:03 +0200

    INTYG-4334: Uppdaterat text efter ändrade krav (label i skicka intyg wizard).

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -91,7 +91,7 @@ angular.module('minaintyg').constant('minaintyg.messages', {
         'btn-link.restore.tooltip': 'Flytta tillbaka intyget till Inkorgen.',
 
         /* SEND */
-        'sendpage.label.select-recipients.heading': 'Välj mottagare du vill skicka intyget till.',
+        'sendpage.label.select-recipients.heading': 'Välj mottagare du vill skicka intyget till:',
         'sendpage.label.none-selected': 'Du har inte valt någon mottagare att skicka intyget till.',
         'sendpage.label.some-selected': 'Du har valt att skicka intyget till följande mottagare:',
         'sendpage.btn.send': 'Skicka intyg',
