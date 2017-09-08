85f945c0d0f269063329328b9cadd908c2526259
Per Beijer <per.beijer@usify.se>
2017-09-08 15:53:49 +0200

    INTYG-4354: Tydligare vy för "Skicka intyg till mottagare" när det bara finns 1 möjlig mottagare

--- web/src/main/webapp/app/messages.js
+++ web/src/main/webapp/app/messages.js
@@ -91,6 +91,7 @@ angular.module('minaintyg').constant('minaintyg.messages', {
         'btn-link.restore.tooltip': 'Flytta tillbaka intyget till Inkorgen.',
 
         /* SEND */
+        'sendpage.label.single-recipient.heading': 'Klicka på knappen om du vill skicka ditt intyg till nedanstående mottagare',
         'sendpage.label.select-recipients.heading': 'Välj mottagare du vill skicka intyget till:',
         'sendpage.label.none-selected': 'Du har inte valt någon mottagare att skicka intyget till.',
         'sendpage.label.some-selected': 'Du har valt att skicka intyget till följande mottagare:',
