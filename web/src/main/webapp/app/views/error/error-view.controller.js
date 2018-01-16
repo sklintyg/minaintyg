eb13af17a60cb5f2a0fd57cc2b09d178fca5476a
Magnus Ekstrand <magnus@ekstrand.net>
2018-01-16 11:51:41 +0100

    Revert "INTYG-5284: uppdatering av licens-plugin till att hantera även *.groovy och *.js"
    This reverts commit 52398fcb9b74460b724521744581823678b304fa.

--- web/src/main/webapp/app/views/error/error-view.controller.js
+++ web/src/main/webapp/app/views/error/error-view.controller.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ * Copyright (C) 2016 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
  * Generic controller that exposes an errorCode extracted from stateParams to
  * the scope. used by the modules as a common component. Make a dependency to
