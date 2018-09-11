26965906a2ebb5c197d1a4a26749683d46f9b5cd
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-09-11 10:59:37 +0200

    INTYG-7080: Ensade namn för att följa våra OCP-konventioner, dvs ej appnamns-prefix på properties

---
+++ devops/openshift/test/env/secret-env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Assign backing service addresses from the outer environment
+
+export REDIS_PASSWORD=${REDIS_PASSWORD:-redis}
+export REDIS_PORT=$REDIS_SERVICE_PORT
+export REDIS_HOST=$REDIS_SERVICE_HOST
+
+# dev profile is default for pipeline
+SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-test,testability-api,caching-enabled,wc-pu-stub}
+
+export CATALINA_OPTS_APPEND="\
+-Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
+-Dconfig.folder=/opt/$APP_NAME/config \
+-Dconfig.file=/opt/$APP_NAME/config/minaintyg.properties \
+-Dlogback.file=classpath:logback-ocp.xml \
+-Dcertificate.folder=/opt/$APP_NAME/env \
+-Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
+-Dresources.folder=/tmp/resources \
+-Dfile.encoding=UTF-8 \
+-DbaseUrl=http://${APP_NAME}:8080"
