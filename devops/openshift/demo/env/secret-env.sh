ac03094be3ba8a684538978ec2f209469ffafe3e
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-10-22 10:51:28 +0200

    BUILDENV: Demo-konfig

---
+++ devops/openshift/demo/env/secret-env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Assign backing service addresses from the outer environment
+
+export REDIS_PASSWORD=${REDIS_PASSWORD:-redis}
+export REDIS_PORT=$REDIS_SERVICE_PORT
+export REDIS_HOST=$REDIS_SERVICE_HOST
+
+# dev profile is default for pipeline
+SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-test,mi-security-test,caching-enabled}
+
+export CATALINA_OPTS_APPEND="\
+-Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
+-Dconfig.folder=/opt/$APP_NAME/config \
+-Dconfig.file=/opt/$APP_NAME/config/minaintyg.properties \
+-Dlogback.file=/opt/$APP_NAME/config/minaintyg-logback.xml \
+-Dcertificate.folder=/opt/$APP_NAME/certifikat \
+-Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
+-Dresources.folder=/tmp/resources \
+-Dfile.encoding=UTF-8 \
+-DbaseUrl=http://${APP_NAME}:8080 \
+-Dcertificate.baseUrl=http://intygstjanst-${STAGE}:8080/ \
+-Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
