ebb153142c9783542b36d151e1faebe6d116c498
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-11-21 13:05:40 +0100

    INTYG-7636: Infört prometheus-loggning i MI inkl exponering av health-metrics

---
+++ web/src/main/java/se/inera/intyg/minaintyg/web/service/HealthMonitor.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.minaintyg.web.service;
+
+import java.util.Collections;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import io.prometheus.client.Gauge;
+import se.riv.itintegration.monitoring.rivtabp21.v1.PingForConfigurationResponderInterface;
+import se.riv.itintegration.monitoring.v1.PingForConfigurationResponseType;
+import se.riv.itintegration.monitoring.v1.PingForConfigurationType;
+
+/**
+ * Exposes health metrics as Prometheus values. To simplify any 3rd party scraping applications, all metrics produced
+ * by this component uses the following conventions:
+ *
+ * All metrics are prefixed with "health_"
+ * All metrics are suffixed with their type, either "_normal" that indicates a boolean value 0 or 1 OR
+ * "_value" that indiciates a numeric metric of some kind.
+ *
+ * Note that NORMAL values uses 0 to indicate OK state and 1 to indicate a problem.
+ *
+ * @author eriklupander
+ */
+@EnableScheduling
+@Component
+public class HealthMonitor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HealthMonitor.class);
+
+    private static final long DELAY = 30000L;
+
+    private static final String PREFIX = "health_";
+    private static final String NORMAL = "_normal";
+    private static final String VALUE = "_value";
+
+    private static final long START_TIME = System.currentTimeMillis();
+
+    private static final Gauge UPTIME = Gauge.build()
+            .name(PREFIX + "uptime" + VALUE)
+            .help("Current uptime in seconds")
+            .register();
+
+    private static final Gauge LOGGED_IN_USERS = Gauge.build()
+            .name(PREFIX + "logged_in_users" + VALUE)
+            .help("Current number of logged in users")
+            .register();
+
+    private static final Gauge IT_ACCESSIBLE = Gauge.build()
+            .name(PREFIX + "intygstjanst_accessible" + NORMAL)
+            .help("0 == OK 1 == NOT OK")
+            .register();
+
+    private static final long MILLIS_PER_SECOND = 1000L;
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${intygstjanst.logicaladdress}")
+    private String intygstjanstLogicalAddress;
+
+    @Autowired
+    @Qualifier("pingIntygstjanstForConfigurationClient")
+    private PingForConfigurationResponderInterface intygstjanstPingForConfiguration;
+
+    @Autowired
+    @Qualifier("rediscache")
+    private RedisTemplate<Object, Object> redisTemplate;
+
+    // Runs a lua script to count number of keys matching our session keys.
+    private RedisScript<Long> redisScript;
+
+    @PostConstruct
+    public void init() {
+        redisScript = new DefaultRedisScript<>(
+                "return #redis.call('keys','spring:session:"  + appName + ":index:*')", Long.class);
+    }
+
+    @Scheduled(fixedDelay = DELAY)
+    public void healthCheck() {
+        long secondsSinceStart = (System.currentTimeMillis() - START_TIME) / MILLIS_PER_SECOND;
+        UPTIME.set(secondsSinceStart);
+        LOGGED_IN_USERS.set(countSessions());
+        IT_ACCESSIBLE.set(pingIntygstjanst() ? 0 : 1);
+    }
+
+    private int countSessions() {
+        if (redisScript == null) {
+            LOG.warn("Trying to count users from Redis before Redis script was initialized.");
+            return -1;
+        }
+        Long numberOfUsers = redisTemplate.execute(redisScript, Collections.emptyList());
+        return numberOfUsers.intValue();
+    }
+
+    private boolean pingIntygstjanst() {
+        try {
+            PingForConfigurationType parameters = new PingForConfigurationType();
+            PingForConfigurationResponseType pingResponse = intygstjanstPingForConfiguration.pingForConfiguration(
+                    intygstjanstLogicalAddress,
+                    parameters);
+            return pingResponse != null;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
