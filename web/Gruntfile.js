b1adedb8b7e6f6d9d085dc304458c0bde6bcfd40
Johan Kindgren <johan.kindgren@callistaenterprise.se>
2018-12-19 10:03:33 +0100

    INTYG-7432: Rättat fel i Gruntfile.js

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function(grunt) {
         'luae_fs':     { base: 'common/fk/luae_fs', angularModule:'luae_fs' },
         'af00213':     { base: 'common/af/af00213', angularModule:'af00213' },
         'af00251':     { base: 'common/af/af00251', angularModule:'af00251' },
-        'ag114':     { base: 'common/skl/ag114', angularModule:'ag114' }
+        'ag114':     { base: 'common/skl/ag114', angularModule:'ag114' },
         'ag7804':     { base: 'common/skl/ag7804', angularModule:'ag7804' }
     };
     Object.keys(modules).forEach(function(moduleName) {
