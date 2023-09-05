0ceb395f48a907eee5133d3ab2dc6c85d02cb822
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-09-05 16:05:56 +0200

    INTYGFV-15815: Cleanup

--- web/src/main/java/se/inera/intyg/minaintyg/MinaintygApplication.java
+++ web/src/main/java/se/inera/intyg/minaintyg/MinaintygApplication.java
@@ -2,8 +2,9 @@ package se.inera.intyg.minaintyg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MinaintygApplication {
 
     public static void main(String[] args) {
--- web/src/main/java/se/inera/intyg/minaintyg/config/WebSecurityConfig.java
+++ web/src/main/java/se/inera/intyg/minaintyg/config/WebSecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
