0319e6e1e70e73bfeade99f15fd20cf33bc4bd92
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-09-11 16:34:46 +0200

    INTYGFV-15840: Removed licence.

--- web/src/main/java/se/inera/intyg/minaintyg/monitoring/MarkerFilter.java
+++ web/src/main/java/se/inera/intyg/minaintyg/monitoring/MarkerFilter.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2023 Inera AB (http://www.inera.se)
- *
- * This file is part of sklintyg (https://github.com/sklintyg).
- *
- * sklintyg is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * sklintyg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package se.inera.intyg.minaintyg.monitoring;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
@@ -31,8 +13,8 @@ import org.slf4j.MarkerFactory;
 
 public class MarkerFilter extends AbstractMatcherFilter<ILoggingEvent> {
 
-  public static final Marker MONITORING = MarkerFactory.getMarker("Monitoring");
-  public static final Marker VALIDATION = MarkerFactory.getMarker("Validation");
+  public static final Marker MONITORING = MarkerFactory.getMarker(LogMarkers.MONITORING.getName());
+  public static final Marker VALIDATION = MarkerFactory.getMarker(LogMarkers.VALIDATION.getName());
 
   List<Marker> markersToMatch = new ArrayList<>();
 
