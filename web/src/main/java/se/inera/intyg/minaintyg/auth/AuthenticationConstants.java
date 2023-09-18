91df410e8df9eee8afef52566033b68efe1a44f8
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-09-18 14:48:43 +0200

    INTYGFV-15823: Adjustments to make it possible to use SAML-login locally in the dev environment.

--- web/src/main/java/se/inera/intyg/minaintyg/auth/AuthenticationConstants.java
+++ web/src/main/java/se/inera/intyg/minaintyg/auth/AuthenticationConstants.java
@@ -4,7 +4,6 @@ public final class AuthenticationConstants {
 
   public static final String PERSON_ID_ATTRIBUTE = "Subject_SerialNumber";
   public static final String ELEG_PARTY_REGISTRATION_ID = "eleg";
-  public static final String SINGLE_LOGOUT_SERVICE_LOCATION = "{baseUrl}/logout/saml2/slo";
   public static final String UNKNOWN_PARTY_REGISTRATION_ID = "Unknown";
 
   private AuthenticationConstants() {
