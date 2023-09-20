b00ff2d8718adadb1ee5183fced4c60cd4f666df
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2023-09-20 15:26:18 +0200

    DEVENV: Added test person Athena React Andersson. (#230)

--- web/src/main/java/se/inera/intyg/minaintyg/testability/TestPersonService.java
+++ web/src/main/java/se/inera/intyg/minaintyg/testability/TestPersonService.java
@@ -13,40 +13,44 @@ public class TestPersonService {
   private List<TestPerson> listOfTestPersons() {
     return List.of(
         TestPerson.builder()
+            .personId("194011306125")
+            .personName("Athena React Andersson ")
+            .build(),
+        TestPerson.builder()
             .personId("198901192396")
-            .personName("Agnarsson Agnarsson")
+            .personName("Agnarsson React Agnarsson")
             .build(),
         TestPerson.builder()
             .personId("200210282398")
-            .personName("Albert Albertsson")
+            .personName("Albert React Albertsson")
             .build(),
         TestPerson.builder()
             .personId("200210292389")
-            .personName("Albertina Alison")
+            .personName("Albertina React Alison")
             .build(),
         TestPerson.builder()
             .personId("197901242391")
-            .personName("Albin Ander")
+            .personName("Albin React Ander")
             .build(),
         TestPerson.builder()
             .personId("194110299221")
-            .personName("Alexa Valfridsson")
+            .personName("Alexa React Valfridsson")
             .build(),
         TestPerson.builder()
             .personId("197901252382")
-            .personName("Aline Andersson")
+            .personName("Aline React Andersson")
             .build(),
         TestPerson.builder()
             .personId("199606282391")
-            .personName("Allan Allanson")
+            .personName("Allan React Allanson")
             .build(),
         TestPerson.builder()
             .personId("199606292382")
-            .personName("Alma Almarsson")
+            .personName("Alma React Almarsson")
             .build(),
         TestPerson.builder()
             .personId("194112128154")
-            .personName("Alve Alfredsson")
+            .personName("Alve React Alfredsson")
             .build()
     );
   }
