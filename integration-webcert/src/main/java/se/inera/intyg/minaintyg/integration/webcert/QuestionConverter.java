d03cb0c63b807d512f2e64fc573921fc74166351
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-10-03 08:26:35 +0200

    INTYGFV-15912: Extract question converter logic to a separate class.

---
+++ integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/QuestionConverter.java
@@ -0,0 +1,66 @@
+package se.inera.intyg.minaintyg.integration.webcert;
+
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getLabelText;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getTitleText;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueBoolean;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueCode;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueCodeList;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueDateListSubQuestions;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueDateRangeList;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueDiagnosisList;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueIcf;
+import static se.inera.intyg.minaintyg.integration.webcert.ValueToolkit.getValueText;
+
+import org.springframework.stereotype.Component;
+import se.inera.intyg.minaintyg.integration.api.certificate.model.CertificateQuestion;
+import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValueText;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataIcfValue;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataTextValue;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueBoolean;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueCode;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueCodeList;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueDateList;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueDateRangeList;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueDiagnosisList;
+
+@Component
+public class QuestionConverter {
+
+  public CertificateQuestion convert(CertificateDataElement element) {
+    final var certificateQuestionBuilder = CertificateQuestion.builder();
+    if (element.getValue() instanceof CertificateDataTextValue) {
+      certificateQuestionBuilder.value(getValueText(element.getValue()));
+    }
+    if (element.getValue() instanceof CertificateDataValueBoolean) {
+      certificateQuestionBuilder.value(getValueBoolean(element.getValue()));
+    }
+    if (element.getValue() instanceof CertificateDataValueDateList) {
+      certificateQuestionBuilder.subQuestions(
+          getValueDateListSubQuestions(element.getValue(), element.getConfig()));
+      certificateQuestionBuilder.value(CertificateQuestionValueText.builder().build());
+    }
+    if (element.getValue() instanceof CertificateDataValueCodeList) {
+      certificateQuestionBuilder.value(getValueCodeList(element.getValue()));
+    }
+    if (element.getValue() instanceof CertificateDataValueDiagnosisList) {
+      certificateQuestionBuilder.value(
+          getValueDiagnosisList(element.getValue(), element.getConfig()));
+    }
+    if (element.getValue() instanceof CertificateDataIcfValue) {
+      certificateQuestionBuilder.value(getValueIcf(element.getValue()));
+    }
+    if (element.getValue() instanceof CertificateDataValueDateRangeList) {
+      certificateQuestionBuilder.value(
+          getValueDateRangeList(element.getValue(), element.getConfig()));
+    }
+    if (element.getValue() instanceof CertificateDataValueCode) {
+      certificateQuestionBuilder.value(getValueCode(element.getValue(), element.getConfig()));
+    }
+
+    return certificateQuestionBuilder
+        .title(getTitleText(element))
+        .label(getLabelText(element))
+        .build();
+  }
+}
