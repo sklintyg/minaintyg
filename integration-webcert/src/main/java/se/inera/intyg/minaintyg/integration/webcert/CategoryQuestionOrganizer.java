8678b28aba751943f4f6af7a9140cbb93f2292c6
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-10-03 07:53:26 +0200

    INTYGFV-15912: Organize categories and questions into a map data structure.

--- integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/CategoryQuestionOrganizer.java
+++ integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/CategoryQuestionOrganizer.java
@@ -2,7 +2,9 @@ package se.inera.intyg.minaintyg.integration.webcert;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigTypes;
@@ -24,6 +26,28 @@ public class CategoryQuestionOrganizer {
       }
     });
     return organizedCertificateDataElements;
+  }
+
+  public Map<CertificateDataElement, List<CertificateDataElement>> organizeAsMap(
+      List<CertificateDataElement> certificateDataElements) {
+    final var elementsSortedByIndex = getElementsSortedByIndex(certificateDataElements);
+    final var organizedCertificateDataElements = new ArrayList<List<CertificateDataElement>>();
+
+    elementsSortedByIndex.forEach(element -> {
+      if (elementIsCategory(element)) {
+        organizedCertificateDataElements.add(new ArrayList<>());
+        addElementToList(organizedCertificateDataElements, element);
+      } else {
+        addElementToList(organizedCertificateDataElements, element);
+      }
+    });
+
+    Map<CertificateDataElement, List<CertificateDataElement>> organizeAsMap = new HashMap<>();
+    organizedCertificateDataElements.forEach(elements ->
+        organizeAsMap.put(elements.get(0), elements.subList(1, elements.size()))
+    );
+
+    return organizeAsMap;
   }
 
   private static void addElementToList(
--- integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/CategoryQuestionOrganizerTest.java
+++ integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/CategoryQuestionOrganizerTest.java
@@ -2,9 +2,11 @@ package se.inera.intyg.minaintyg.integration.webcert;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfig;
@@ -115,6 +117,112 @@ class CategoryQuestionOrganizerTest {
     );
     assertThrows(IllegalArgumentException.class,
         () -> categoryQuestionOrganizer.organize(certificateDataElements));
+  }
+
+  @Nested
+  class OrganizeAsMap {
+
+    @Test
+    void shouldOrganizeElementsByIndex() {
+      final var certificateDataElements = List.of(
+          createElement(TEXT_TYPE, 1, "1", "0"),
+          createElement(BOOLEAN_TYPE, 4, "4", "0"),
+          createElement(TEXT_TYPE, 2, "2", "0"),
+          createElement(BOOLEAN_TYPE, 3, "3", "0"),
+          createElement(CATEGORY_TYPE, 0, "0", null)
+      );
+      final var expectedResult = Map.of(
+          createElement(CATEGORY_TYPE, 0, "0", null),
+          List.of(
+              createElement(TEXT_TYPE, 1, "1", "0"),
+              createElement(TEXT_TYPE, 2, "2", "0"),
+              createElement(BOOLEAN_TYPE, 3, "3", "0"),
+              createElement(BOOLEAN_TYPE, 4, "4", "0")
+          )
+      );
+      final var result = categoryQuestionOrganizer.organizeAsMap(certificateDataElements);
+      assertMap(expectedResult, result);
+    }
+
+    @Test
+    void shouldGroupElementsByCategory() {
+      final var certificateDataElements = List.of(
+          createElement(TEXT_TYPE, 1, "1", "0"),
+          createElement(BOOLEAN_TYPE, 4, "4", "2"),
+          createElement(CATEGORY_TYPE, 2, "2", null),
+          createElement(BOOLEAN_TYPE, 3, "3", "2"),
+          createElement(CATEGORY_TYPE, 0, "0", null)
+      );
+      final var expectedResult = Map.of(
+          createElement(CATEGORY_TYPE, 0, "0", null),
+          List.of(
+              createElement(TEXT_TYPE, 1, "1", "0")
+          ),
+          createElement(CATEGORY_TYPE, 2, "2", null),
+          List.of(
+              createElement(BOOLEAN_TYPE, 3, "3", "2"),
+              createElement(BOOLEAN_TYPE, 4, "4", "2")
+          )
+      );
+      final var result = categoryQuestionOrganizer.organizeAsMap(certificateDataElements);
+      assertMap(expectedResult, result);
+    }
+
+    @Test
+    void shouldGroupElementsByMultipleCategories() {
+      final var certificateDataElements = List.of(
+          createElement(TEXT_TYPE, 1, "1", "0"),
+          createElement(BOOLEAN_TYPE, 4, "4", "2"),
+          createElement(CATEGORY_TYPE, 2, "2", null),
+          createElement(BOOLEAN_TYPE, 3, "3", "2"),
+          createElement(CATEGORY_TYPE, 0, "0", null),
+          createElement(CATEGORY_TYPE, 5, "5", null),
+          createElement(BOOLEAN_TYPE, 6, "6", "5"),
+          createElement(CATEGORY_TYPE, 7, "7", null),
+          createElement(BOOLEAN_TYPE, 8, "8", "7")
+      );
+      final var expectedResult = Map.of(
+          createElement(CATEGORY_TYPE, 0, "0", null),
+          List.of(
+              createElement(TEXT_TYPE, 1, "1", "0")
+          ),
+          createElement(CATEGORY_TYPE, 2, "2", null),
+          List.of(
+              createElement(BOOLEAN_TYPE, 3, "3", "2"),
+              createElement(BOOLEAN_TYPE, 4, "4", "2")
+          ),
+          createElement(CATEGORY_TYPE, 5, "5", null),
+          List.of(
+              createElement(BOOLEAN_TYPE, 6, "6", "5")
+          ),
+          createElement(CATEGORY_TYPE, 7, "7", null),
+          List.of(
+              createElement(BOOLEAN_TYPE, 8, "8", "7")
+          )
+      );
+      final var result = categoryQuestionOrganizer.organizeAsMap(certificateDataElements);
+      assertMap(expectedResult, result);
+    }
+
+    @Test
+    void shouldThrowIfQuestionDoesNotHaveACategoryParent() {
+      final var certificateDataElements = List.of(
+          createElement(TEXT_TYPE, 1, "1", null),
+          createElement(BOOLEAN_TYPE, 4, "1", null)
+      );
+      assertThrows(IllegalArgumentException.class,
+          () -> categoryQuestionOrganizer.organizeAsMap(certificateDataElements));
+    }
+
+    void assertMap(Map<CertificateDataElement, List<CertificateDataElement>> expected,
+        Map<CertificateDataElement, List<CertificateDataElement>> actual) {
+      expected.keySet().forEach(expectedCategory -> {
+            assertTrue(actual.containsKey(expectedCategory),
+                "Missing category: '%s' ".formatted(expectedCategory));
+            assertEquals(expected.get(expectedCategory), actual.get(expectedCategory));
+          }
+      );
+    }
   }
 
   private static CertificateDataElement createElement(String type, int index, String id,
