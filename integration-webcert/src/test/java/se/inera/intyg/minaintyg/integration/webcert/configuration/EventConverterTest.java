4b21a4432528369b16834de8dfa11cfb3df75005
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-10-05 10:37:11 +0200

    INTYGFV-15910: Fix test

--- integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/configuration/EventConverterTest.java
+++ integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/configuration/EventConverterTest.java
@@ -15,213 +15,213 @@ import se.inera.intyg.minaintyg.integration.webcert.client.dto.metadata.Certific
 
 class EventConverterTest {
 
-    private final EventConverter eventConverter = new EventConverter();
+  private final EventConverter eventConverter = new EventConverter();
 
-    @Test
-    void shallConvertSentCertificateToSentEvent() {
-        final var expectedEvent = List.of(
-            CertificateEvent.builder()
-                .description("Skickat till recipientName")
+  @Test
+  void shallConvertSentCertificateToSentEvent() {
+    final var expectedEvent = List.of(
+        CertificateEvent.builder()
+            .description("Skickat till recipientName")
+            .build()
+    );
+
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .sent(true)
+        .sentTo("recipientName")
+        .build();
+
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(expectedEvent, actualEvents);
+  }
+
+  @Test
+  void shallReturnEmptyListIfUnsentCertificate() {
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .sent(false)
+        .sentTo("recipientName")
+        .build();
+
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(Collections.emptyList(), actualEvents);
+  }
+
+  @Test
+  void shallConvertReplacedCertificateToReplacedEvent() {
+    final var expectedEvent = List.of(
+        CertificateEvent.builder()
+            .certificateId("id")
+            .description("Ersatt av vården med ett nytt intyg")
+            .timestamp(LocalDateTime.now())
+            .build()
+    );
+
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .relations(
+            CertificateRelations.builder()
+                .children(
+                    new CertificateRelation[]{
+                        CertificateRelation.builder()
+                            .type(CertificateRelationType.REPLACED)
+                            .certificateId(expectedEvent.get(0).getCertificateId())
+                            .created(expectedEvent.get(0).getTimestamp())
+                            .status(CertificateStatus.SIGNED)
+                            .build()
+                    }
+                )
                 .build()
-        );
+        )
+        .build();
 
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .sent(true)
-            .sentTo("recipientName")
-            .build();
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(expectedEvent, actualEvents);
+  }
 
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(expectedEvent, actualEvents);
-    }
+  @Test
+  void shallConvertComplementedCertificateToReplacedEvent() {
+    final var expectedEvent = List.of(
+        CertificateEvent.builder()
+            .certificateId("id")
+            .description("Ersatt av vården med ett nytt intyg")
+            .timestamp(LocalDateTime.now())
+            .build()
+    );
 
-    @Test
-    void shallReturnEmptyListIfUnsentCertificate() {
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .sent(false)
-            .sentTo("recipientName")
-            .build();
-
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(Collections.emptyList(), actualEvents);
-    }
-
-    @Test
-    void shallConvertReplacedCertificateToReplacedEvent() {
-        final var expectedEvent = List.of(
-            CertificateEvent.builder()
-                .certificateId("id")
-                .description("Ersatt av vården med ett nytt intyg")
-                .timestamp(LocalDateTime.now())
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .relations(
+            CertificateRelations.builder()
+                .children(
+                    new CertificateRelation[]{
+                        CertificateRelation.builder()
+                            .type(CertificateRelationType.COMPLEMENTED)
+                            .certificateId(expectedEvent.get(0).getCertificateId())
+                            .created(expectedEvent.get(0).getTimestamp())
+                            .status(CertificateStatus.SIGNED)
+                            .build()
+                    }
+                )
                 .build()
-        );
+        )
+        .build();
 
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .relations(
-                CertificateRelations.builder()
-                    .children(
-                        new CertificateRelation[]{
-                            CertificateRelation.builder()
-                                .type(CertificateRelationType.REPLACED)
-                                .certificateId(expectedEvent.get(0).getCertificateId())
-                                .created(expectedEvent.get(0).getTimestamp())
-                                .status(CertificateStatus.SIGNED)
-                                .build()
-                        }
-                    )
-                    .build()
-            )
-            .build();
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(expectedEvent, actualEvents);
+  }
 
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(expectedEvent, actualEvents);
-    }
+  @Test
+  void shallConvertReplacingCertificateToReplacesEvent() {
+    final var expectedEvent = List.of(
+        CertificateEvent.builder()
+            .certificateId("id")
+            .description("Ersätter ett intyg som inte längre är aktuellt")
+            .timestamp(LocalDateTime.now())
+            .build()
+    );
 
-    @Test
-    void shallConvertComplementedCertificateToReplacedEvent() {
-        final var expectedEvent = List.of(
-            CertificateEvent.builder()
-                .certificateId("id")
-                .description("Ersatt av vården med ett nytt intyg")
-                .timestamp(LocalDateTime.now())
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .relations(
+            CertificateRelations.builder()
+                .parent(CertificateRelation.builder()
+                    .type(CertificateRelationType.REPLACED)
+                    .certificateId(expectedEvent.get(0).getCertificateId())
+                    .created(expectedEvent.get(0).getTimestamp())
+                    .status(CertificateStatus.SIGNED)
+                    .build())
                 .build()
-        );
+        )
+        .build();
 
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .relations(
-                CertificateRelations.builder()
-                    .children(
-                        new CertificateRelation[]{
-                            CertificateRelation.builder()
-                                .type(CertificateRelationType.COMPLEMENTED)
-                                .certificateId(expectedEvent.get(0).getCertificateId())
-                                .created(expectedEvent.get(0).getTimestamp())
-                                .status(CertificateStatus.SIGNED)
-                                .build()
-                        }
-                    )
-                    .build()
-            )
-            .build();
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(expectedEvent, actualEvents);
+  }
 
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(expectedEvent, actualEvents);
-    }
+  @Test
+  void shallConvertComplementedCertificateToReplacesEvent() {
+    final var expectedEvent = List.of(
+        CertificateEvent.builder()
+            .certificateId("id")
+            .description("Ersätter ett intyg som inte längre är aktuellt")
+            .timestamp(LocalDateTime.now())
+            .build()
+    );
 
-    @Test
-    void shallConvertReplacingCertificateToReplacesEvent() {
-        final var expectedEvent = List.of(
-            CertificateEvent.builder()
-                .certificateId("id")
-                .description("Ersätter ett intyg som inte längre är aktuellt")
-                .timestamp(LocalDateTime.now())
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .relations(
+            CertificateRelations.builder()
+                .parent(CertificateRelation.builder()
+                    .type(CertificateRelationType.COMPLEMENTED)
+                    .certificateId(expectedEvent.get(0).getCertificateId())
+                    .created(expectedEvent.get(0).getTimestamp())
+                    .status(CertificateStatus.SIGNED)
+                    .build())
                 .build()
-        );
+        )
+        .build();
 
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .relations(
-                CertificateRelations.builder()
-                    .parent(CertificateRelation.builder()
-                        .type(CertificateRelationType.REPLACED)
-                        .certificateId(expectedEvent.get(0).getCertificateId())
-                        .created(expectedEvent.get(0).getTimestamp())
-                        .status(CertificateStatus.SIGNED)
-                        .build())
-                    .build()
-            )
-            .build();
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(expectedEvent, actualEvents);
+  }
 
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(expectedEvent, actualEvents);
-    }
+  @Test
+  void shallConvertReplacingCertificateThatHasBeenReplacedToReplacedEvent() {
+    final var expectedEvent = List.of(
+        CertificateEvent.builder()
+            .certificateId("id")
+            .description("Ersätter ett intyg som inte längre är aktuellt")
+            .timestamp(LocalDateTime.now())
+            .build(),
+        CertificateEvent.builder()
+            .certificateId("id")
+            .description("Ersatt av vården med ett nytt intyg")
+            .timestamp(LocalDateTime.now())
+            .build());
 
-    @Test
-    void shallConvertComplementedCertificateToReplacesEvent() {
-        final var expectedEvent = List.of(
-            CertificateEvent.builder()
-                .certificateId("id")
-                .description("Ersätter ett intyg som inte längre är aktuellt")
-                .timestamp(LocalDateTime.now())
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .relations(
+            CertificateRelations.builder()
+                .parent(CertificateRelation.builder()
+                    .type(CertificateRelationType.REPLACED)
+                    .certificateId(expectedEvent.get(0).getCertificateId())
+                    .created(expectedEvent.get(0).getTimestamp())
+                    .status(CertificateStatus.SIGNED)
+                    .build())
+                .children(
+                    new CertificateRelation[]{
+                        CertificateRelation.builder()
+                            .type(CertificateRelationType.REPLACED)
+                            .certificateId(expectedEvent.get(1).getCertificateId())
+                            .created(expectedEvent.get(1).getTimestamp())
+                            .status(CertificateStatus.SIGNED)
+                            .build()
+                    }
+                )
                 .build()
-        );
+        )
+        .build();
 
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .relations(
-                CertificateRelations.builder()
-                    .parent(CertificateRelation.builder()
-                        .type(CertificateRelationType.COMPLEMENTED)
-                        .certificateId(expectedEvent.get(0).getCertificateId())
-                        .created(expectedEvent.get(0).getTimestamp())
-                        .status(CertificateStatus.SIGNED)
-                        .build())
-                    .build()
-            )
-            .build();
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(expectedEvent, actualEvents);
+  }
 
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(expectedEvent, actualEvents);
-    }
+  @Test
+  void shallReturnEmptyListIfReplacingCertificateIsRevoked() {
+    final var metadataDTO = CertificateMetadataDTO.builder()
+        .relations(
+            CertificateRelations.builder()
+                .children(
+                    new CertificateRelation[]{
+                        CertificateRelation.builder()
+                            .type(CertificateRelationType.REPLACED)
+                            .certificateId("id")
+                            .created(LocalDateTime.now())
+                            .status(CertificateStatus.REVOKED)
+                            .build()
+                    }
+                )
+                .build()
+        )
+        .build();
 
-    @Test
-    void shallConvertReplacingCertificateThatHasBeenReplacedToReplacedEvent() {
-        final var expectedEvent = List.of(
-            CertificateEvent.builder()
-                .certificateId("id")
-                .description("Ersätter ett intyg som inte längre är aktuellt")
-                .timestamp(LocalDateTime.now())
-                .build(),
-            CertificateEvent.builder()
-                .certificateId("id")
-                .description("Ersatt av vården med ett nytt intyg")
-                .timestamp(LocalDateTime.now())
-                .build());
-
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .relations(
-                CertificateRelations.builder()
-                    .parent(CertificateRelation.builder()
-                        .type(CertificateRelationType.REPLACED)
-                        .certificateId(expectedEvent.get(0).getCertificateId())
-                        .created(expectedEvent.get(0).getTimestamp())
-                        .status(CertificateStatus.SIGNED)
-                        .build())
-                    .children(
-                        new CertificateRelation[]{
-                            CertificateRelation.builder()
-                                .type(CertificateRelationType.REPLACED)
-                                .certificateId(expectedEvent.get(0).getCertificateId())
-                                .created(expectedEvent.get(0).getTimestamp())
-                                .status(CertificateStatus.SIGNED)
-                                .build()
-                        }
-                    )
-                    .build()
-            )
-            .build();
-
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(expectedEvent, actualEvents);
-    }
-
-    @Test
-    void shallReturnEmptyListIfReplacingCertificateIsRevoked() {
-        final var metadataDTO = CertificateMetadataDTO.builder()
-            .relations(
-                CertificateRelations.builder()
-                    .children(
-                        new CertificateRelation[]{
-                            CertificateRelation.builder()
-                                .type(CertificateRelationType.REPLACED)
-                                .certificateId("id")
-                                .created(LocalDateTime.now())
-                                .status(CertificateStatus.REVOKED)
-                                .build()
-                        }
-                    )
-                    .build()
-            )
-            .build();
-
-        final var actualEvents = eventConverter.convert(metadataDTO);
-        assertEquals(Collections.emptyList(), actualEvents);
-    }
+    final var actualEvents = eventConverter.convert(metadataDTO);
+    assertEquals(Collections.emptyList(), actualEvents);
+  }
 }
