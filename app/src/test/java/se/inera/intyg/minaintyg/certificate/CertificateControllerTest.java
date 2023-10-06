764285ee9619bc89b9e0cf9bc23f3413918de2c0
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-10-06 10:32:30 +0200

    INTYGFV-15925: Added total number of certificates to filter response (#246)

--- app/src/test/java/se/inera/intyg/minaintyg/certificate/CertificateControllerTest.java
+++ app/src/test/java/se/inera/intyg/minaintyg/certificate/CertificateControllerTest.java
@@ -144,6 +144,7 @@ class CertificateControllerTest {
         .years(List.of("2020"))
         .certificateTypes(List.of(CertificateTypeFilter.builder().build()))
         .units(List.of(CertificateUnit.builder().build()))
+        .total(5)
         .build();
 
     @BeforeEach
@@ -180,6 +181,13 @@ class CertificateControllerTest {
         final var response = certificateController.getCertificateListFilter();
 
         assertEquals(EXPECTED_RESPONSE.getStatuses(), response.getStatuses());
+      }
+
+      @Test
+      void shouldSetTotal() {
+        final var response = certificateController.getCertificateListFilter();
+
+        assertEquals(EXPECTED_RESPONSE.getTotal(), response.getTotal());
       }
     }
   }
