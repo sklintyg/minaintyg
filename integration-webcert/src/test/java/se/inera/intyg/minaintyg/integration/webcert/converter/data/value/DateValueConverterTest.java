8c5b185f4f53fd5adac26aad742391c674d219b0
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-10-10 14:37:32 +0200

    INTYGFV-15932: Value converter for Date

---
+++ integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/DateValueConverterTest.java
@@ -0,0 +1,65 @@
+package se.inera.intyg.minaintyg.integration.webcert.converter.data.value;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static se.inera.intyg.minaintyg.integration.webcert.converter.data.value.ValueConverter.NOT_PROVIDED_VALUE;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValueText;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataTextValue;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueDate;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueType;
+
+class DateValueConverterTest {
+
+  private final ValueConverter dateValueConverter = new DateValueConverter();
+
+  @Test
+  void shallReturnDateValueType() {
+    assertEquals(CertificateDataValueType.DATE, dateValueConverter.getType());
+  }
+
+  @Test
+  void shallReturnNotProvidedValueIfNull() {
+    final var element = CertificateDataElement.builder()
+        .value(
+            CertificateDataValueDate.builder()
+                .build()
+        )
+        .build();
+
+    final var actualValue = dateValueConverter.convert(element);
+    assertEquals(NOT_PROVIDED_VALUE, actualValue);
+  }
+
+  @Test
+  void shallReturnTextValueWithDateIfDateExists() {
+    final var expectedValue = CertificateQuestionValueText.builder()
+        .value("2023-01-01")
+        .build();
+
+    final var element = CertificateDataElement.builder()
+        .value(
+            CertificateDataValueDate.builder()
+                .date(LocalDate.parse(expectedValue.getValue()))
+                .build()
+        )
+        .build();
+
+    final var actualValue = dateValueConverter.convert(element);
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  void shallThrowIllegalArgumentIfWrongType() {
+    final var element = CertificateDataElement.builder()
+        .value(
+            CertificateDataTextValue.builder().build()
+        )
+        .build();
+
+    assertThrows(IllegalArgumentException.class, () -> dateValueConverter.convert(element));
+  }
+}
