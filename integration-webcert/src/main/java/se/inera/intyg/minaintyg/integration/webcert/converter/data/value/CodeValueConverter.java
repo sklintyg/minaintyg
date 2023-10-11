9de2f6a71444a84a73b1c7cc709e5a4078a1dc83
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-10-11 11:24:35 +0200

    INTYGFV-15932: Value converter for Code with handling for different type of config types

--- integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/CodeValueConverter.java
+++ integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/CodeValueConverter.java
@@ -1,11 +1,20 @@
 package se.inera.intyg.minaintyg.integration.webcert.converter.data.value;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValue;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValueText;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigDropdown;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigRadioMultipleCodeOptionalDropdown;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.DropdownItem;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.RadioMultipleCodeOptionalDropdown;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValue;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueCode;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueType;
 
@@ -18,25 +27,106 @@ public class CodeValueConverter extends AbstractValueConverter {
   }
 
   @Override
-  public CertificateQuestionValue convertToValue(CertificateDataElement element) {
-    if (!(element.getConfig() instanceof final CertificateDataConfigRadioMultipleCodeOptionalDropdown dataConfig)) {
-      return notProvidedTextValue();
-    }
-    final var dataValue = (CertificateDataValueCode) element.getValue();
-    final var radioMultipleCodeOptionalDropdownLabel = dataConfig.getList().stream()
-        .filter(config -> config.getId().equals(dataValue.getCode()))
-        .map(RadioMultipleCodeOptionalDropdown::getLabel)
-        .toList();
+  public boolean includeSubquestions() {
+    return true;
+  }
 
+  @Override
+  public CertificateQuestionValue convertToValue(CertificateDataElement element) {
+    return convert(element, Collections.emptyList());
+  }
+
+  @Override
+  public CertificateQuestionValue convert(CertificateDataElement element,
+      List<CertificateDataElement> subQuestions) {
+    final var codeValue = getCodeValue(element.getValue());
+    if (codeValue.isEmpty() || codeValue.get().getId() == null) {
+      return NOT_PROVIDED_VALUE;
+    }
+
+    return codeValue.map(value ->
+            switch (element.getConfig().getType()) {
+              case UE_DROPDOWN -> convertDropdown(
+                  value,
+                  (CertificateDataConfigDropdown) element.getConfig()
+              );
+              case UE_RADIO_MULTIPLE_CODE_OPTIONAL_DROPDOWN -> convertRadioMultipleCodeOptionalDropdown(
+                  value,
+                  (CertificateDataConfigRadioMultipleCodeOptionalDropdown) element.getConfig(),
+                  subQuestions
+              );
+              default -> createTextValue(value.getId());
+            }
+        )
+        .orElse(NOT_PROVIDED_VALUE);
+  }
+
+  private static CertificateQuestionValueText convertDropdown(
+      CertificateDataValueCode codeValue, CertificateDataConfigDropdown dropdownConfig) {
+    final var idToLabelMap = dropdownConfig.getList().stream()
+        .collect(
+            Collectors.toMap(
+                DropdownItem::getId,
+                DropdownItem::getLabel
+            )
+        );
+    return createTextValue(
+        idToLabelMap.getOrDefault(codeValue.getId(), codeValue.getId())
+    );
+  }
+
+  private CertificateQuestionValueText convertRadioMultipleCodeOptionalDropdown(
+      CertificateDataValueCode value,
+      CertificateDataConfigRadioMultipleCodeOptionalDropdown dataConfig,
+      List<CertificateDataElement> subQuestions) {
+
+    final var radioMultipleCodeOptionalDropdown = getIdToLabelMap(dataConfig).get(value.getId());
+    if (radioMultipleCodeOptionalDropdown == null) {
+      return createTextValue(
+          value.getId()
+      );
+    }
+
+    if (radioMultipleCodeOptionalDropdown.getDropdownQuestionId() == null) {
+      return createTextValue(
+          radioMultipleCodeOptionalDropdown.getLabel()
+      );
+    }
+
+    final var subQuestionLabel = subQuestions.stream()
+        .filter(subQuestion -> subQuestion.getId().equalsIgnoreCase(
+            radioMultipleCodeOptionalDropdown.getDropdownQuestionId()))
+        .findAny()
+        .map(this::convertToValue)
+        .map(textValue -> ((CertificateQuestionValueText) textValue).getValue())
+        .orElse(radioMultipleCodeOptionalDropdown.getDropdownQuestionId());
+
+    return createTextValue(
+        "%s %s".formatted(radioMultipleCodeOptionalDropdown.getLabel(), subQuestionLabel)
+    );
+  }
+
+  private static Map<String, RadioMultipleCodeOptionalDropdown> getIdToLabelMap(
+      CertificateDataConfigRadioMultipleCodeOptionalDropdown dataConfig) {
+    return dataConfig.getList().stream()
+        .collect(
+            Collectors.toMap(
+                RadioMultipleCodeOptionalDropdown::getId,
+                Function.identity()
+            )
+        );
+  }
+
+  private static CertificateQuestionValueText createTextValue(String value) {
     return CertificateQuestionValueText.builder()
-        .value(radioMultipleCodeOptionalDropdownLabel.isEmpty() ? NOT_PROVIDED
-            : radioMultipleCodeOptionalDropdownLabel.get(0))
+        .value(value)
         .build();
   }
 
-  private static CertificateQuestionValueText notProvidedTextValue() {
-    return CertificateQuestionValueText.builder()
-        .value(NOT_PROVIDED)
-        .build();
+  private Optional<CertificateDataValueCode> getCodeValue(CertificateDataValue value) {
+    if (value instanceof CertificateDataValueCode codeValue) {
+      return Optional.of(codeValue);
+    }
+    return Optional.empty();
   }
 }
