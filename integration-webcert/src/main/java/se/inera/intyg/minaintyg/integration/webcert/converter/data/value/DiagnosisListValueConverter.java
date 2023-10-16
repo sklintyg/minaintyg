d331909fa3fbb9838e028155a1e6c46b6b766769
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2023-10-16 05:59:43 +0200

    INTYGFV-15954: Value converter for Diagnoses (#254)

--- integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/DiagnosisListValueConverter.java
+++ integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/DiagnosisListValueConverter.java
@@ -1,17 +1,25 @@
 package se.inera.intyg.minaintyg.integration.webcert.converter.data.value;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValue;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValueTable;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfig;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigDiagnoses;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.DiagnosesTerminology;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValue;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueDiagnosis;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueDiagnosisList;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueType;
 
 @Component
 public class DiagnosisListValueConverter extends AbstractValueConverter {
+
+  public static final String EMPTY = "";
+  public static final String MISSING = "Saknas";
 
   @Override
   public CertificateDataValueType getType() {
@@ -20,25 +28,61 @@ public class DiagnosisListValueConverter extends AbstractValueConverter {
 
   @Override
   public CertificateQuestionValue convertToValue(CertificateDataElement element) {
-    final var diagnosesTerminologies = ((CertificateDataConfigDiagnoses) element.getConfig()).getTerminology();
-    final var certificateDataValueDiagnoses = ((CertificateDataValueDiagnosisList) element.getValue()).getList();
+    final var terminologies = getTerminologies(element.getConfig());
+    final var diagnoses = getDiagnoses(element.getValue());
+    if (diagnoses.isEmpty() || diagnoses.get().isEmpty()) {
+      return NOT_PROVIDED_VALUE;
+    }
 
-    final var headings = diagnosesTerminologies.stream()
-        .map(DiagnosesTerminology::getLabel)
-        .toList();
-
-    final var values = certificateDataValueDiagnoses.stream()
-        .map(diagnosis -> List.of(diagnosis.getCode(), diagnosis.getDescription()))
-        .toList();
-
-    return buildQuestionValueTable(headings, values);
+    return CertificateQuestionValueTable.builder()
+        .headings(
+            getHeadings(diagnoses.get(), terminologies.orElse(Collections.emptyList()))
+        )
+        .values(
+            getValues(diagnoses.get())
+        )
+        .build();
   }
 
-  private static CertificateQuestionValueTable buildQuestionValueTable(List<String> headings,
-      List<List<String>> values) {
-    return CertificateQuestionValueTable.builder()
-        .headings(headings)
-        .values(values)
-        .build();
+  private static List<String> getHeadings(List<CertificateDataValueDiagnosis> diagnoses,
+      List<DiagnosesTerminology> terminologies) {
+    return List.of(
+        getTerminologyLabel(diagnoses, terminologies),
+        EMPTY
+    );
+  }
+
+  private static String getTerminologyLabel(List<CertificateDataValueDiagnosis> diagnoses,
+      List<DiagnosesTerminology> terminologies) {
+    return diagnoses.stream().findFirst()
+        .map(CertificateDataValueDiagnosis::getTerminology)
+        .map(terminologyId ->
+            terminologies.stream()
+                .filter(terminology -> terminology.getId().equalsIgnoreCase(terminologyId))
+                .findAny()
+                .map(DiagnosesTerminology::getLabel)
+                .orElse(terminologyId)
+        )
+        .orElse(MISSING);
+  }
+
+  private static List<List<String>> getValues(List<CertificateDataValueDiagnosis> diagnoses) {
+    return diagnoses.stream()
+        .map(diagnosis -> List.of(diagnosis.getCode(), diagnosis.getDescription()))
+        .toList();
+  }
+
+  private Optional<List<DiagnosesTerminology>> getTerminologies(CertificateDataConfig config) {
+    if (config instanceof CertificateDataConfigDiagnoses configDiagnoses) {
+      return Optional.ofNullable(configDiagnoses.getTerminology());
+    }
+    return Optional.empty();
+  }
+
+  private Optional<List<CertificateDataValueDiagnosis>> getDiagnoses(CertificateDataValue value) {
+    if (value instanceof CertificateDataValueDiagnosisList diagnosisList) {
+      return Optional.ofNullable(diagnosisList.getList());
+    }
+    return Optional.empty();
   }
 }
