fe7de395a6cbad304db47b6646511f30b7fcc953
Mikaela Romild <mikaela.romild@soprasteria.com>
2023-10-18 12:54:12 +0200

    INTYGFV-15962: Value converter for IcfValues

--- integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/IcfValueConverter.java
+++ integration-webcert/src/main/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/IcfValueConverter.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValue;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValueText;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigIcf;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataIcfValue;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValueType;
 
@@ -17,9 +18,18 @@ public class IcfValueConverter extends AbstractValueConverter {
 
   @Override
   public CertificateQuestionValue convertToValue(CertificateDataElement element) {
-    final var value = ((CertificateDataIcfValue) element.getValue()).getText();
-    // TODO: Handle icf-codes
-    return getText(value);
+    final var icfCodes = ((CertificateDataIcfValue) element.getValue()).getIcfCodes();
+    final var text = ((CertificateDataIcfValue) element.getValue()).getText();
+
+    if (icfCodes != null) {
+      final var collectionsLabel = ((CertificateDataConfigIcf) element.getConfig()).getCollectionsLabel();
+      final var value = collectionsLabel + "\n" + icfCodes + "\n" + "\n" + text;
+
+      return CertificateQuestionValueText.builder()
+          .value(value)
+          .build();
+    }
+    return getText(text);
   }
 
   private static CertificateQuestionValueText getText(String value) {
--- integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/IcfValueConverterTest.java
+++ integration-webcert/src/test/java/se/inera/intyg/minaintyg/integration/webcert/converter/data/value/IcfValueConverterTest.java
@@ -2,18 +2,23 @@ package se.inera.intyg.minaintyg.integration.webcert.converter.data.value;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.value.CertificateQuestionValueText;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.CertificateDataElement;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfig;
-import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigTextArea;
+import se.inera.intyg.minaintyg.integration.webcert.client.dto.config.CertificateDataConfigIcf;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataIcfValue;
 import se.inera.intyg.minaintyg.integration.webcert.client.dto.value.CertificateDataValue;
 
 class IcfValueConverterTest {
 
   private static final String TEXT_VALUE = "textValue";
+  private static final String COLLECTIONS_LABEL = "collectionsLabel";
   private static final String NOT_PROVIDED = "Ej angivet";
+  private static final String ICF_CODE_1 = "icfCode1";
+  private static final String ICF_CODE_2 = "icfCode2";
+
   private ValueConverter icfValueConverter = new IcfValueConverter();
 
   @Test
@@ -21,7 +26,7 @@ class IcfValueConverterTest {
     final var value = CertificateDataIcfValue.builder()
         .text(TEXT_VALUE)
         .build();
-    final var config = CertificateDataConfigTextArea.builder().build();
+    final var config = createConfig();
     final var element = createElement(config, value);
     final var expectedValue = CertificateQuestionValueText.builder()
         .value(TEXT_VALUE)
@@ -33,13 +38,67 @@ class IcfValueConverterTest {
   @Test
   void shouldConvertCertificateIcfNoValue() {
     final var value = CertificateDataIcfValue.builder().build();
-    final var config = CertificateDataConfigTextArea.builder().build();
+    final var config = createConfig();
     final var element = createElement(config, value);
     final var expectedValue = CertificateQuestionValueText.builder()
         .value(NOT_PROVIDED)
         .build();
     final var result = icfValueConverter.convert(element);
     assertEquals(expectedValue, result);
+  }
+
+  @Test
+  void shouldReturnCollectionsLabelIfOneIcfCodeIsProvided() {
+    final var value = CertificateDataIcfValue.builder()
+        .text(TEXT_VALUE)
+        .icfCodes(List.of(ICF_CODE_1))
+        .build();
+    final var config = createConfig();
+    final var element = createElement(config, value);
+    final var valueString =
+        COLLECTIONS_LABEL + "\n" + List.of(ICF_CODE_1) + "\n" + "\n" + TEXT_VALUE;
+    final var expectedValue = CertificateQuestionValueText.builder()
+        .value(valueString)
+        .build();
+    final var result = icfValueConverter.convert(element);
+    assertEquals(expectedValue, result);
+  }
+
+  @Test
+  void shouldReturnCollectionsLabelIfMultipleIcfCodesAreProvided() {
+    final var value = CertificateDataIcfValue.builder()
+        .text(TEXT_VALUE)
+        .icfCodes(List.of(ICF_CODE_1, ICF_CODE_2))
+        .build();
+    final var config = createConfig();
+    final var element = createElement(config, value);
+    final var valueString =
+        COLLECTIONS_LABEL + "\n" + List.of(ICF_CODE_1, ICF_CODE_2) + "\n" + "\n" + TEXT_VALUE;
+    final var expectedValue = CertificateQuestionValueText.builder()
+        .value(valueString)
+        .build();
+    final var result = icfValueConverter.convert(element);
+    assertEquals(expectedValue, result);
+  }
+
+  @Test
+  void shouldNotReturnCollectionsLabelIfNoIcfCodeIsProvided() {
+    final var value = CertificateDataIcfValue.builder()
+        .text(TEXT_VALUE)
+        .build();
+    final var config = createConfig();
+    final var element = createElement(config, value);
+    final var expectedValue = CertificateQuestionValueText.builder()
+        .value(TEXT_VALUE)
+        .build();
+    final var result = icfValueConverter.convert(element);
+    assertEquals(expectedValue, result);
+  }
+
+  private static CertificateDataConfigIcf createConfig() {
+    return CertificateDataConfigIcf.builder()
+        .collectionsLabel(COLLECTIONS_LABEL)
+        .build();
   }
 
   private static CertificateDataElement createElement(CertificateDataConfig config,
