f95250d0bbd27abf680abb8744382987a776ef03
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-10-24 10:38:48 +0200

    INTYGFV-15993: Refactored

--- app/src/main/java/se/inera/intyg/minaintyg/certificate/service/SendCertificateService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/certificate/service/SendCertificateService.java
@@ -10,7 +10,6 @@ import se.inera.intyg.minaintyg.integration.api.certificate.GetCertificateIntegr
 import se.inera.intyg.minaintyg.integration.api.certificate.SendCertificateIntegrationRequest;
 import se.inera.intyg.minaintyg.integration.api.certificate.SendCertificateIntegrationService;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.Certificate;
-import se.inera.intyg.minaintyg.integration.api.certificate.model.common.CertificateRecipient;
 import se.inera.intyg.minaintyg.integration.api.certificate.model.common.CertificateStatusType;
 import se.inera.intyg.minaintyg.logging.MonitoringLogService;
 import se.inera.intyg.minaintyg.user.UserService;
@@ -38,29 +37,20 @@ public class SendCertificateService {
     );
   }
 
-  private static boolean isReplaced(Certificate certificate) {
-    final var statuses = certificate.getMetadata().getStatuses();
-    return statuses != null
-        && statuses.stream().anyMatch(status -> status == CertificateStatusType.REPLACED);
-  }
-
-
   private void sendCertificate(
       SendCertificateRequest request,
       MinaIntygUser user,
       Certificate certificate
   ) {
 
-    final var recipient = certificate.getMetadata().getRecipient();
-
-    validateAction(recipient, isReplaced(certificate));
+    validateAction(certificate);
 
     sendCertificateIntegrationService.send(
         SendCertificateIntegrationRequest
             .builder()
             .certificateId(request.getCertificateId())
             .patientId(user.getPersonId())
-            .recipient(recipient.getId())
+            .recipient(certificate.getMetadata().getRecipient().getId())
             .build()
     );
   }
@@ -74,8 +64,16 @@ public class SendCertificateService {
     );
   }
 
-  private void validateAction(CertificateRecipient recipient, boolean isReplaced) {
-    if (isReplaced) {
+  private static boolean isReplaced(Certificate certificate) {
+    final var statuses = certificate.getMetadata().getStatuses();
+    return statuses != null
+        && statuses.stream().anyMatch(status -> status == CertificateStatusType.REPLACED);
+  }
+
+  private void validateAction(Certificate certificate) {
+    final var recipient = certificate.getMetadata().getRecipient();
+
+    if (isReplaced(certificate)) {
       throw new IllegalStateException("Cannot send replaced certificate");
     }
 
