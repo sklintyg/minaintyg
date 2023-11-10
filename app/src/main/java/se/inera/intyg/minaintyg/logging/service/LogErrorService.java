d332905d447f3bc4e9ae3dd79297b26ed4a4bf77
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-11-10 08:35:27 +0100

    INTYGFV-16065: Addressed comments in PR

--- app/src/main/java/se/inera/intyg/minaintyg/logging/service/LogErrorService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/logging/service/LogErrorService.java
@@ -3,20 +3,14 @@ package se.inera.intyg.minaintyg.logging.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.minaintyg.logging.service.dto.LogErrorRequest;
-import se.inera.intyg.minaintyg.user.UserService;
 
 @RequiredArgsConstructor
 @Service
 public class LogErrorService {
 
   private final MonitoringLogService monitoringLogService;
-  private final UserService userService;
 
   public void log(LogErrorRequest request) {
-    if (userService.getLoggedInUser().isEmpty()) {
-      throw new IllegalStateException("Error cannot be logged if user is not defined");
-    }
-
     monitoringLogService.logClientError(
         request.getId(),
         request.getCode(),
--- app/src/test/java/se/inera/intyg/minaintyg/error/service/LogErrorServiceTest.java
+++ app/src/test/java/se/inera/intyg/minaintyg/error/service/LogErrorServiceTest.java
@@ -1,25 +1,18 @@
 package se.inera.intyg.minaintyg.error.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import se.inera.intyg.minaintyg.auth.MinaIntygUser;
 import se.inera.intyg.minaintyg.logging.service.LogErrorService;
 import se.inera.intyg.minaintyg.logging.service.MonitoringLogService;
 import se.inera.intyg.minaintyg.logging.service.dto.LogErrorRequest;
-import se.inera.intyg.minaintyg.user.UserService;
 
 @ExtendWith(MockitoExtension.class)
 class LogErrorServiceTest {
@@ -34,84 +27,66 @@ class LogErrorServiceTest {
   @Mock
   MonitoringLogService monitoringLogService;
 
-  @Mock
-  UserService userService;
-
   @InjectMocks
   private LogErrorService logErrorService;
 
   @Test
-  void shouldThrowErrorIfUserIsNotDefined() {
-    assertThrows(IllegalStateException.class, () -> logErrorService.log(REQUEST));
+  void shouldLogUsingId() {
+    final var captor = ArgumentCaptor.forClass(String.class);
+
+    logErrorService.log(REQUEST);
+
+    verify(monitoringLogService).logClientError(
+        captor.capture(),
+        anyString(),
+        anyString(),
+        anyString()
+    );
+    assertEquals(REQUEST.getId(), captor.getValue());
   }
 
-  @Nested
-  class HasUser {
+  @Test
+  void shouldLogUsingCode() {
+    final var captor = ArgumentCaptor.forClass(String.class);
 
-    @BeforeEach
-    void setup() {
-      when(userService.getLoggedInUser())
-          .thenReturn(Optional.of(MinaIntygUser.builder().build()));
-    }
+    logErrorService.log(REQUEST);
 
-    @Test
-    void shouldLogUsingId() {
-      final var captor = ArgumentCaptor.forClass(String.class);
+    verify(monitoringLogService).logClientError(
+        anyString(),
+        captor.capture(),
+        anyString(),
+        anyString()
+    );
+    assertEquals(REQUEST.getCode(), captor.getValue());
+  }
 
-      logErrorService.log(REQUEST);
+  @Test
+  void shouldLogUsingMessage() {
+    final var captor = ArgumentCaptor.forClass(String.class);
 
-      verify(monitoringLogService).logClientError(
-          captor.capture(),
-          anyString(),
-          anyString(),
-          anyString()
-      );
-      assertEquals(REQUEST.getId(), captor.getValue());
-    }
+    logErrorService.log(REQUEST);
 
-    @Test
-    void shouldLogUsingCode() {
-      final var captor = ArgumentCaptor.forClass(String.class);
+    verify(monitoringLogService).logClientError(
+        anyString(),
+        anyString(),
+        captor.capture(),
+        anyString()
+    );
+    assertEquals(REQUEST.getMessage(), captor.getValue());
+  }
 
-      logErrorService.log(REQUEST);
+  @Test
+  void shouldLogUsingStackTrace() {
+    final var captor = ArgumentCaptor.forClass(String.class);
 
-      verify(monitoringLogService).logClientError(
-          anyString(),
-          captor.capture(),
-          anyString(),
-          anyString()
-      );
-      assertEquals(REQUEST.getCode(), captor.getValue());
-    }
+    logErrorService.log(REQUEST);
 
-    @Test
-    void shouldLogUsingMessage() {
-      final var captor = ArgumentCaptor.forClass(String.class);
-
-      logErrorService.log(REQUEST);
-
-      verify(monitoringLogService).logClientError(
-          anyString(),
-          anyString(),
-          captor.capture(),
-          anyString()
-      );
-      assertEquals(REQUEST.getMessage(), captor.getValue());
-    }
-
-    @Test
-    void shouldLogUsingStackTrace() {
-      final var captor = ArgumentCaptor.forClass(String.class);
-
-      logErrorService.log(REQUEST);
-
-      verify(monitoringLogService).logClientError(
-          anyString(),
-          anyString(),
-          anyString(),
-          captor.capture()
-      );
-      assertEquals(REQUEST.getStackTrace(), captor.getValue());
-    }
+    verify(monitoringLogService).logClientError(
+        anyString(),
+        anyString(),
+        anyString(),
+        captor.capture()
+    );
+    assertEquals(REQUEST.getStackTrace(), captor.getValue());
   }
 }
