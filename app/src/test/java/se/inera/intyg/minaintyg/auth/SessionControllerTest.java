e761e95d716a89eaf6ff1c050718fe2ad7db1ab0
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-11-13 12:19:19 +0100

    INTYGFV-16079: Clean up

--- app/src/test/java/se/inera/intyg/minaintyg/auth/SessionControllerTest.java
+++ app/src/test/java/se/inera/intyg/minaintyg/auth/SessionControllerTest.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2023 Inera AB (http://www.inera.se)
- *
- * This file is part of sklintyg (https://github.com/sklintyg).
- *
- * sklintyg is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * sklintyg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package se.inera.intyg.minaintyg.auth;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -39,51 +20,51 @@ import org.springframework.security.core.context.SecurityContext;
 @ExtendWith(MockitoExtension.class)
 class SessionControllerTest {
 
-    @InjectMocks
-    private SessionController sessionController;
+  @InjectMocks
+  private SessionController sessionController;
 
-    private static HttpServletRequest request;
+  private static HttpServletRequest request;
 
-    @Nested
-    class WithSession {
+  @Nested
+  class WithSession {
 
-        @BeforeEach
-        void setup() {
-            request = mock(HttpServletRequest.class);
-            final var context = mock(SecurityContext.class);
-            final var authentication = mock(Authentication.class);
-            final var session = mock(HttpSession.class);
+    @BeforeEach
+    void setup() {
+      request = mock(HttpServletRequest.class);
+      final var context = mock(SecurityContext.class);
+      final var authentication = mock(Authentication.class);
+      final var session = mock(HttpSession.class);
 
-            when(request.getSession((false))).thenReturn(session);
-            when(session.getAttribute(anyString())).thenReturn(context);
-            when(context.getAuthentication()).thenReturn(authentication);
-            when(authentication.getPrincipal()).thenReturn(MinaIntygUser.builder().build());
-        }
-
-        @Test
-        void testGetSessionWhenSession() {
-            final var sessionStatus = sessionController.getSessionStatus(request);
-
-            assertTrue(sessionStatus.isHasSession());
-        }
-
+      when(request.getSession((false))).thenReturn(session);
+      when(session.getAttribute(anyString())).thenReturn(context);
+      when(context.getAuthentication()).thenReturn(authentication);
+      when(authentication.getPrincipal()).thenReturn(MinaIntygUser.builder().build());
     }
 
-    @Nested
-    class NoSession {
+    @Test
+    void testGetSessionWhenSession() {
+      final var sessionStatus = sessionController.getSessionStatus(request);
 
-        @BeforeEach
-        void setup() {
-            request = mock(HttpServletRequest.class);
-            when(request.getSession((false))).thenReturn(null);
-        }
-
-        @Test
-        void testGetSessionStatusWhenNoSession() {
-            final var sessionStatus = sessionController.getSessionStatus(request);
-
-            assertFalse(sessionStatus.isHasSession());
-        }
+      assertTrue(sessionStatus.isHasSession());
     }
+
+  }
+
+  @Nested
+  class NoSession {
+
+    @BeforeEach
+    void setup() {
+      request = mock(HttpServletRequest.class);
+      when(request.getSession((false))).thenReturn(null);
+    }
+
+    @Test
+    void testGetSessionStatusWhenNoSession() {
+      final var sessionStatus = sessionController.getSessionStatus(request);
+
+      assertFalse(sessionStatus.isHasSession());
+    }
+  }
 
 }
