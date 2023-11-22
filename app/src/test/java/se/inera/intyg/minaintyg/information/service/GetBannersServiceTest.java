78edace182300e5426d546da0139b6ce5fc42245
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-11-22 09:53:22 +0100

    INTYGFV-16120: Updated test

--- app/src/test/java/se/inera/intyg/minaintyg/information/service/GetBannersServiceTest.java
+++ app/src/test/java/se/inera/intyg/minaintyg/information/service/GetBannersServiceTest.java
@@ -2,6 +2,7 @@ package se.inera.intyg.minaintyg.information.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,7 +22,11 @@ import se.inera.intyg.minaintyg.integration.api.banner.model.Banner;
 @ExtendWith(MockitoExtension.class)
 class GetBannersServiceTest {
 
-  private static final List<Banner> EXPECTED_BANNERS = List.of(Banner.builder().build());
+  private static final Banner EXPECTED_BANNER = Banner.builder().build();
+  private static final List<Banner> EXPECTED_BANNERS = List.of(
+      EXPECTED_BANNER,
+      EXPECTED_BANNER
+  );
   private static final FormattedBanner EXPECTED_FORMATTED_BANNER = FormattedBanner.builder()
       .build();
   @Mock
@@ -52,7 +57,7 @@ class GetBannersServiceTest {
 
     getBannersService.get();
 
-    verify(formattedBannerConverter).convert(captor.capture());
+    verify(formattedBannerConverter, times(2)).convert(captor.capture());
     assertEquals(EXPECTED_BANNERS.get(0), captor.getValue());
   }
 
@@ -60,6 +65,6 @@ class GetBannersServiceTest {
   void shouldReturnFormattedBanners() {
     final var response = getBannersService.get();
 
-    assertEquals(List.of(EXPECTED_FORMATTED_BANNER), response);
+    assertEquals(List.of(EXPECTED_FORMATTED_BANNER, EXPECTED_FORMATTED_BANNER), response);
   }
 }
