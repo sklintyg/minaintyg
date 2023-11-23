8f40312bbc93bbc1769a8cc8323c1d5ea9e5d000
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-11-23 11:10:21 +0100

    INTYGFV-16121: Remove redundant format for BannerDTO since its no longer stored in Redis.

--- integration-intygsadmin/src/main/java/se/inera/intyg/minaintyg/integration/intygsadmin/client/dto/BannerDTO.java
+++ integration-intygsadmin/src/main/java/se/inera/intyg/minaintyg/integration/intygsadmin/client/dto/BannerDTO.java
@@ -1,10 +1,5 @@
 package se.inera.intyg.minaintyg.integration.intygsadmin.client.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -24,21 +19,11 @@ public class BannerDTO implements Serializable {
 
   @Serial
   private static final long serialVersionUID = 1L;
-  public static final String FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
   private UUID id;
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonFormat(pattern = FORMAT)
   private LocalDateTime createdAt;
   private Application application;
   private String message;
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonFormat(pattern = FORMAT)
   private LocalDateTime displayFrom;
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonFormat(pattern = FORMAT)
   private LocalDateTime displayTo;
   private BannerPriority priority;
 }
