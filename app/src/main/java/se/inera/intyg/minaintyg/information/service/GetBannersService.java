eb12d57f428671c96ab2f07045f26d23dad4ee21
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2023-11-27 11:14:18 +0100

    INTYGFV-16121: Use BannerRepository instead of GetBannerIntegrationService. (#311)

--- app/src/main/java/se/inera/intyg/minaintyg/information/service/GetBannersService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/information/service/GetBannersService.java
@@ -4,17 +4,16 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.minaintyg.information.service.dto.FormattedBanner;
-import se.inera.intyg.minaintyg.integration.api.banner.GetBannerIntegrationService;
 
 @Service
 @RequiredArgsConstructor
 public class GetBannersService {
 
-  private final GetBannerIntegrationService getBannerIntegrationService;
+  private final BannerRepository bannerRepository;
   private final FormattedBannerConverter formattedBannerConverter;
 
   public List<FormattedBanner> get() {
-    final var response = getBannerIntegrationService.get();
+    final var response = bannerRepository.get();
 
     return response.getBanners()
         .stream()
