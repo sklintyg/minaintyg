1c28bbf06b0659356ca5903b7ea4920d6baf7a79
mikeyOne <55154460+mikeyOne@users.noreply.github.com>
2024-06-12 20:03:46 +0200

    INTYGFV-16645: Added 16-year age limit for login. (#337)

--- app/src/main/java/se/inera/intyg/minaintyg/auth/MinaIntygUserDetailService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/auth/MinaIntygUserDetailService.java
@@ -1,16 +1,26 @@
 package se.inera.intyg.minaintyg.auth;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import se.inera.intyg.minaintyg.exception.LoginAgeLimitException;
 import se.inera.intyg.minaintyg.integration.api.person.GetPersonIntegrationRequest;
 import se.inera.intyg.minaintyg.integration.api.person.GetPersonIntegrationService;
 import se.inera.intyg.minaintyg.integration.api.person.model.Status;
+import se.inera.intyg.minaintyg.util.HashUtility;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MinaIntygUserDetailService {
+
+
+  @Value("${login.age.limit}")
+  private long loginAgeLimit;
 
   private final GetPersonIntegrationService getPersonIntegrationService;
 
@@ -21,18 +31,26 @@ public class MinaIntygUserDetailService {
             .personId(personId)
             .build()
     );
+
     if (!personResponse.getStatus().equals(Status.FOUND)) {
       handleCommunicationFault(personResponse.getStatus());
     }
+
+    final var personIdFromResponse = personResponse.getPerson().getPersonId();
+
+    if (belowLoginAgeLimit(personIdFromResponse)) {
+      handleUnderagePerson(personIdFromResponse);
+    }
+
     return MinaIntygUser.builder()
-        .personId(personResponse.getPerson().getPersonId())
+        .personId(personIdFromResponse)
         .personName(personResponse.getPerson().getName())
         .loginMethod(loginMethod)
         .build();
   }
 
   private static void handleCommunicationFault(Status status) {
-    log.error("Error communicating with IntygProxyService, status from response: '{}", status);
+    log.error("Error communicating with IntygProxyService, status from response: '{}'", status);
     throw new IllegalStateException(
         "Error communication with IntygProxyService. Status: '%s' ".formatted(status)
     );
@@ -44,5 +62,32 @@ public class MinaIntygUserDetailService {
           "personId must have a valid value: '%s'".formatted(personId)
       );
     }
+  }
+
+  private boolean belowLoginAgeLimit(String personId) {
+    String birthDate = personId.substring(0, 8);
+    final var dayOfMonth = Integer.parseInt(birthDate.substring(6, 8));
+
+    if (isCoordinationNumber(dayOfMonth)) {
+      birthDate = handleCoordinationNumber(birthDate, dayOfMonth);
+    }
+
+    final var date = LocalDate.parse(birthDate, DateTimeFormatter.BASIC_ISO_DATE);
+    return date.plusYears(loginAgeLimit).isAfter(LocalDate.now(ZoneId.systemDefault()));
+  }
+
+  private boolean isCoordinationNumber(int dayOfMonth) {
+    return dayOfMonth > 60;
+  }
+
+  private String handleCoordinationNumber(String birthDate, int dayOfMonth) {
+    return birthDate.substring(0, 6).concat(String.valueOf(dayOfMonth - 60));
+  }
+
+  private void handleUnderagePerson(String personId) {
+    final var errorMessage = "Access denied for underage person with id '%s'."
+        .formatted(HashUtility.hash(personId));
+    log.warn(errorMessage);
+    throw new LoginAgeLimitException(errorMessage);
   }
 }
--- app/src/test/java/se/inera/intyg/minaintyg/auth/MinaIntygUserDetailServiceTest.java
+++ app/src/test/java/se/inera/intyg/minaintyg/auth/MinaIntygUserDetailServiceTest.java
@@ -1,33 +1,52 @@
 package se.inera.intyg.minaintyg.auth;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import se.inera.intyg.minaintyg.exception.LoginAgeLimitException;
 import se.inera.intyg.minaintyg.integration.api.person.GetPersonIntegrationRequest;
 import se.inera.intyg.minaintyg.integration.api.person.GetPersonIntegrationResponse;
 import se.inera.intyg.minaintyg.integration.api.person.GetPersonIntegrationService;
 import se.inera.intyg.minaintyg.integration.api.person.model.Person;
 import se.inera.intyg.minaintyg.integration.api.person.model.Status;
+import se.inera.intyg.minaintyg.util.HashUtility;
 
 @ExtendWith(MockitoExtension.class)
 class MinaIntygUserDetailServiceTest {
 
   private static final String PERSON_ID = "191212121212";
   private static final String PERSON_NAME = "Arnold Johansson";
+  private static final long LOGIN_AGE_LIMIT = 16L;
+
   @Mock
   private GetPersonIntegrationService getPersonIntegrationService;
+
   @InjectMocks
   private MinaIntygUserDetailService minaIntygUserDetailService;
 
+  @BeforeEach
+  void setup() {
+    ReflectionTestUtils.setField(minaIntygUserDetailService, "loginAgeLimit", LOGIN_AGE_LIMIT);
+  }
+
   @Test
-  void shouldThrowIlligalArgumentExceptionIfNoPersonIdIsNull() {
+  void shouldThrowIllegalArgumentExceptionIfPersonIdIsNull() {
     assertThrows(IllegalArgumentException.class,
         () -> minaIntygUserDetailService.buildPrincipal(null, LoginMethod.ELVA77));
   }
@@ -51,7 +70,7 @@ class MinaIntygUserDetailServiceTest {
   }
 
   @Test
-  void shouldThrowIlligalArgumentExceptionIfNoPersonIdIsEmtpy() {
+  void shouldThrowIllegalArgumentExceptionIfPersonIdIsEmpty() {
     assertThrows(IllegalArgumentException.class, () -> minaIntygUserDetailService.buildPrincipal("",
         LoginMethod.ELVA77));
   }
@@ -59,8 +78,8 @@ class MinaIntygUserDetailServiceTest {
   @Test
   void shouldReturnTypeMinaIntygUser() {
     final var puResponse = getPersonResponse(Status.FOUND);
-    when(getPersonIntegrationService.getPerson(any(GetPersonIntegrationRequest.class))).thenReturn(
-        puResponse);
+    when(getPersonIntegrationService.getPerson(any(GetPersonIntegrationRequest.class)))
+        .thenReturn(puResponse);
     final var principal = minaIntygUserDetailService.buildPrincipal(PERSON_ID, LoginMethod.ELVA77);
     assertEquals(principal.getClass(), MinaIntygUser.class);
   }
@@ -96,12 +115,106 @@ class MinaIntygUserDetailServiceTest {
     assertEquals(expectedUser.getLoginMethod(), principal.getLoginMethod());
   }
 
+  @Nested
+  class LoginAgeLimit {
+
+    @Test
+    void shouldNotThrowLoginAgeLimitExceptionIfAboveAgeLimit() {
+      final var personId = getPersonId(-1);
+      assertDoesNotThrow(
+          () -> minaIntygUserDetailService.buildPrincipal(personId, LoginMethod.ELVA77));
+    }
+
+    @Test
+    void shouldNotThrowLoginAgeLimitExceptionIfAboveAgeLimitForCoordinationNumber() {
+      final var coordinationNumber = getCoordinationNumber(-1);
+      assertDoesNotThrow(
+          () -> minaIntygUserDetailService.buildPrincipal(coordinationNumber, LoginMethod.ELVA77));
+    }
+
+    @Test
+    void shouldNotThrowLoginAgeLimitExceptionIfExactlyAgeLimit() {
+      final var personId = getPersonId(0);
+      assertDoesNotThrow(
+          () -> minaIntygUserDetailService.buildPrincipal(personId, LoginMethod.ELVA77));
+    }
+
+    @Test
+    void shouldNotThrowLoginAgeLimitExceptionIfExactlyAgeLimitForCoordinationNumber() {
+      final var coordinationNumber = getCoordinationNumber(0);
+      assertDoesNotThrow(
+          () -> minaIntygUserDetailService.buildPrincipal(coordinationNumber, LoginMethod.ELVA77));
+    }
+
+    @Test
+    void shouldThrowLoginAgeLimitExceptionIfBelowAgeLimit() {
+      final var personId = getPersonId(1);
+      assertThrows(LoginAgeLimitException.class,
+          () -> minaIntygUserDetailService.buildPrincipal(personId, LoginMethod.ELVA77));
+    }
+
+    @Test
+    void shouldThrowLoginAgeLimitExceptionIfBelowAgeLimitForCoordinationNumber() {
+      final var getCoordinationNumber = getCoordinationNumber(1);
+      assertThrows(LoginAgeLimitException.class,
+          () -> minaIntygUserDetailService.buildPrincipal(getCoordinationNumber, LoginMethod.ELVA77));
+    }
+
+    @Test
+    void shouldHashPersonIdForLogMessage() {
+      final var personId = getPersonId(1);
+      final var e = assertThrows(LoginAgeLimitException.class,
+          () -> minaIntygUserDetailService.buildPrincipal(personId, LoginMethod.ELVA77));
+
+      assertFalse(e.getMessage().contains(personId));
+      assertTrue(e.getMessage().contains(HashUtility.hash(personId)));
+    }
+
+    @Test
+    void shouldCheckAgeLimitAgainstPersonIdFromPU() {
+      final var personIdResponse = LocalDate.now(ZoneId.systemDefault()).minusYears(LOGIN_AGE_LIMIT)
+          .plusDays(1).format(DateTimeFormatter.BASIC_ISO_DATE).concat("1234");
+
+      when(getPersonIntegrationService.getPerson(any(GetPersonIntegrationRequest.class)))
+          .thenReturn(getPersonResponse(personIdResponse));
+
+      assertThrows(LoginAgeLimitException.class,
+          () -> minaIntygUserDetailService.buildPrincipal(PERSON_ID, LoginMethod.ELVA77));
+    }
+
+    private String getPersonId(int plusDays) {
+      final var personId = LocalDate.now(ZoneId.systemDefault()).minusYears(LOGIN_AGE_LIMIT)
+          .plusDays(plusDays).format(DateTimeFormatter.BASIC_ISO_DATE).concat("1234");
+      when(getPersonIntegrationService.getPerson(any(GetPersonIntegrationRequest.class)))
+          .thenReturn(getPersonResponse(personId));
+      return personId;
+    }
+
+    private String getCoordinationNumber(int plusDays) {
+      final var birthDate = LocalDate.now(ZoneId.systemDefault()).minusYears(LOGIN_AGE_LIMIT)
+          .plusDays(plusDays).format(DateTimeFormatter.BASIC_ISO_DATE);
+      final var dayOfBirth = Integer.parseInt(birthDate.substring(6, 8));
+      final var coordinationNumber= birthDate.substring(0, 6) + (dayOfBirth + 60) + "1234";
+      when(getPersonIntegrationService.getPerson(any(GetPersonIntegrationRequest.class)))
+          .thenReturn(getPersonResponse(coordinationNumber));
+      return coordinationNumber;
+    }
+  }
+
+  private static GetPersonIntegrationResponse getPersonResponse(String personId) {
+    return getPersonResponse(Status.FOUND, personId);
+  }
+
   private static GetPersonIntegrationResponse getPersonResponse(Status status) {
+    return getPersonResponse(status, PERSON_ID);
+  }
+
+  private static GetPersonIntegrationResponse getPersonResponse(Status status, String personId) {
     return GetPersonIntegrationResponse.builder()
         .person(
             Person.builder()
                 .name(PERSON_NAME)
-                .personId(PERSON_ID)
+                .personId(personId)
                 .build()
         )
         .status(status)
