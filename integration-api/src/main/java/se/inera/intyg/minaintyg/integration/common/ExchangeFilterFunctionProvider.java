8c85c9f78f8ec203bc61bff98ff00052ad071154
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2024-10-09 08:29:00 +0200

    K1J-392: Add sessionId and traceId as headers to all external api calls

--- integration-api/src/main/java/se/inera/intyg/minaintyg/integration/common/ExchangeFilterFunctionProvider.java
+++ integration-api/src/main/java/se/inera/intyg/minaintyg/integration/common/ExchangeFilterFunctionProvider.java
@@ -1,9 +1,9 @@
 package se.inera.intyg.minaintyg.integration.common;
 
-import static se.inera.intyg.minaintyg.integration.common.constants.MDCLogConstants.LOG_SESSION_ID_HEADER;
-import static se.inera.intyg.minaintyg.integration.common.constants.MDCLogConstants.LOG_TRACE_ID_HEADER;
-import static se.inera.intyg.minaintyg.integration.common.constants.MDCLogConstants.MDC_SESSION_ID_KEY;
-import static se.inera.intyg.minaintyg.integration.common.constants.MDCLogConstants.MDC_TRACE_ID_KEY;
+import static se.inera.intyg.minaintyg.logging.MdcHelper.LOG_SESSION_ID_HEADER;
+import static se.inera.intyg.minaintyg.logging.MdcHelper.LOG_TRACE_ID_HEADER;
+import static se.inera.intyg.minaintyg.logging.MdcLogConstants.SESSION_ID_KEY;
+import static se.inera.intyg.minaintyg.logging.MdcLogConstants.TRACE_ID_KEY;
 
 import org.slf4j.MDC;
 import org.springframework.web.reactive.function.client.ClientRequest;
@@ -20,8 +20,8 @@ public class ExchangeFilterFunctionProvider {
     return ExchangeFilterFunction.ofRequestProcessor(
         request -> Mono.just(ClientRequest.from(request)
             .headers(httpHeaders -> {
-              httpHeaders.add(LOG_TRACE_ID_HEADER, MDC.get(MDC_TRACE_ID_KEY));
-              httpHeaders.add(LOG_SESSION_ID_HEADER, MDC.get(MDC_SESSION_ID_KEY));
+              httpHeaders.add(LOG_TRACE_ID_HEADER, MDC.get(TRACE_ID_KEY));
+              httpHeaders.add(LOG_SESSION_ID_HEADER, MDC.get(SESSION_ID_KEY));
             })
             .build())
     );
