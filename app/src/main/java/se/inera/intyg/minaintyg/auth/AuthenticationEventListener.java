94dc2af391d8497f5c944d2fbf4a7937da9ffc6d
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2025-04-16 16:34:28 +0200

    K1J-998: Update MDC with new session.id after login is successful (#353)

--- app/src/main/java/se/inera/intyg/minaintyg/auth/AuthenticationEventListener.java
+++ app/src/main/java/se/inera/intyg/minaintyg/auth/AuthenticationEventListener.java
@@ -1,12 +1,18 @@
 package se.inera.intyg.minaintyg.auth;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.authentication.event.LogoutSuccessEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import se.inera.intyg.minaintyg.logging.MdcLogConstants;
 import se.inera.intyg.minaintyg.logging.service.MonitoringLogService;
 
 @Component
@@ -18,6 +24,8 @@ public class AuthenticationEventListener {
 
   @EventListener
   public void onLoginSuccess(InteractiveAuthenticationSuccessEvent success) {
+    updateMDCWithNewSessionId();
+
     final var minaIntygUser = getMinaIntygUser(success.getAuthentication().getPrincipal());
     minaIntygUser.ifPresent(user ->
         monitoringLogService.logUserLogin(
@@ -25,6 +33,26 @@ public class AuthenticationEventListener {
             user.getLoginMethod().value()
         )
     );
+  }
+
+  /**
+   * Spring Security will by default invalidate the old session and create a new one after
+   * authentication. It’s a security feature to protect against session fixation attacks. Update the
+   * MDC with the new session id.
+   */
+  private static void updateMDCWithNewSessionId() {
+    final var attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    if (attrs != null) {
+      final var request = attrs.getRequest();
+      final var session = request.getSession(false);
+
+      if (session != null && session.getId() != null) {
+        final var sessionId = session.getId();
+        final var encodedSessionId = Base64.getEncoder().encodeToString(sessionId.getBytes(
+            StandardCharsets.UTF_8));
+        MDC.put(MdcLogConstants.SESSION_ID_KEY, encodedSessionId);
+      }
+    }
   }
 
   @EventListener
