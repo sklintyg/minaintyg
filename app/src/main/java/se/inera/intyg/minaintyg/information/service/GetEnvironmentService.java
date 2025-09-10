8c40bbce8a371c4f039bd4991b9a897245243ebc
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-09-10 15:27:33 +0200

    K1J-1273: Update environment property key in GetEnvironmentService to use application.environment

--- app/src/main/java/se/inera/intyg/minaintyg/information/service/GetEnvironmentService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/information/service/GetEnvironmentService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GetEnvironmentService {
 
-  @Value("${environment:prod}")
+  @Value("${application.environment:prod}")
   private String environmentType;
 
 
