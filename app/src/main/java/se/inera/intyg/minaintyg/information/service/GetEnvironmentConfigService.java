a57859d06f4209a59aeeedd9a274f7e56d9c30d5
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-09-11 17:18:39 +0200

    K1J-1362: Refactor GetEnvironmentConfigService to utilize the new repository class.

--- app/src/main/java/se/inera/intyg/minaintyg/information/service/GetEnvironmentConfigService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/information/service/GetEnvironmentConfigService.java
@@ -1,15 +1,22 @@
 package se.inera.intyg.minaintyg.information.service;
 
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import se.inera.intyg.minaintyg.information.dto.DynamicLink;
 
 @Service
+@RequiredArgsConstructor
 public class GetEnvironmentConfigService {
 
+  @Getter
   @Value("${application.environment}")
   private String environmentType;
+  private final EnvironmentConfigRepository environmentConfigRepository;
 
-  public String get() {
-    return environmentType;
+  public Map<String, DynamicLink> get() {
+    return environmentConfigRepository.get();
   }
 }
