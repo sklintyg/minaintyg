2a825b4e7b58afc3dc0ad8661638fa7b50a1c5bf
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-09-11 17:29:20 +0200

    K1J-1362: Update GetEnvironmentConfigService to return formatted dynamic links

--- app/src/main/java/se/inera/intyg/minaintyg/information/service/GetEnvironmentConfigService.java
+++ app/src/main/java/se/inera/intyg/minaintyg/information/service/GetEnvironmentConfigService.java
@@ -1,11 +1,14 @@
 package se.inera.intyg.minaintyg.information.service;
 
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.minaintyg.information.dto.DynamicLink;
+import se.inera.intyg.minaintyg.information.dto.FormattedDynamicLink;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +19,17 @@ public class GetEnvironmentConfigService {
   private String environmentType;
   private final EnvironmentConfigRepository environmentConfigRepository;
 
-  public Map<String, DynamicLink> get() {
-    return environmentConfigRepository.get();
+  public Map<String, FormattedDynamicLink> get() {
+    return environmentConfigRepository.get().values().stream()
+        .map(this::getFormattedDynamicLink)
+        .collect(Collectors.toMap(FormattedDynamicLink::getId, Function.identity()));
+  }
+
+  private FormattedDynamicLink getFormattedDynamicLink(DynamicLink link) {
+    return FormattedDynamicLink.builder()
+        .id(link.getId())
+        .name(link.getName())
+        .url(link.getUrls().get(environmentType))
+        .build();
   }
 }
