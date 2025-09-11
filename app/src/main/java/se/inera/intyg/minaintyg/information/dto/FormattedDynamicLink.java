8a2294599baf4d9d0959ad2c094f543a1005316e
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-09-11 17:28:41 +0200

    K1J-1362: Add appropriate instance field variables to FormattedDynamicLink.

--- app/src/main/java/se/inera/intyg/minaintyg/information/dto/FormattedDynamicLink.java
+++ app/src/main/java/se/inera/intyg/minaintyg/information/dto/FormattedDynamicLink.java
@@ -1,5 +1,13 @@
 package se.inera.intyg.minaintyg.information.dto;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
 public class FormattedDynamicLink {
 
+  String id;
+  String name;
+  String url;
 }
