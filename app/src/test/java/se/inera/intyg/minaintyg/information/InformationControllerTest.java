e4b8e2f471f928ec5dc1c45af5d608d6d598ef71
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-09-11 08:39:19 +0200

    K1J-1273: Add environment configuration support to information API (#356)
    * K1J-1273: Add EnvironmentConfig class to manage environment type configuration
    
    * K1J-1273: Add environment field to InformationResponseDTO for configuration purposes
    
    * K1J-1273: Include environment type in InformationResponseDTO response
    
    * K1J-1273: Set environment to staging in application-dev.properties
    
    * K1J-1273: Add tests for environment configuration in InformationController
    
    * K1J-1273: Add GetEnvironmentService and update InformationController to retrieve environment type
    
    * K1J-1273: Add unit tests for GetEnvironmentService to validate environment retrieval
    
    * K1J-1273: Update environment property key in GetEnvironmentService to use application.environment
    
    * K1J-1273: Refactor GetEnvironmentService to simplify get() method logic
    
    * K1J-1273: Put application.environment in alphabetical order.
    
    * K1J-1273: Refactor GetEnvironmentService to remove blank check and update unit tests for improved environment retrieval

--- app/src/test/java/se/inera/intyg/minaintyg/information/InformationControllerTest.java
+++ app/src/test/java/se/inera/intyg/minaintyg/information/InformationControllerTest.java
@@ -12,12 +12,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import se.inera.intyg.minaintyg.information.dto.FormattedBanner;
 import se.inera.intyg.minaintyg.information.dto.InformationResponseDTO;
 import se.inera.intyg.minaintyg.information.service.GetBannersService;
+import se.inera.intyg.minaintyg.information.service.GetEnvironmentService;
 
 @ExtendWith(MockitoExtension.class)
 class InformationControllerTest {
 
   private static final List<FormattedBanner> EXPECTED_BANNERS = List.of(
       FormattedBanner.builder().build());
+  private static final String EXPECTED_ENVIRONMENT = "staging";
 
   private static final InformationResponseDTO EXPECTED_RESPONSE = InformationResponseDTO.builder()
       .banners(EXPECTED_BANNERS)
@@ -25,6 +27,8 @@ class InformationControllerTest {
 
   @Mock
   GetBannersService getBannersService;
+  @Mock
+  GetEnvironmentService getEnvironmentService;
 
   @InjectMocks
   InformationController informationController;
@@ -36,5 +40,30 @@ class InformationControllerTest {
     final var response = informationController.getInformation();
 
     assertEquals(EXPECTED_RESPONSE, response);
+  }
+
+  @Test
+  void shouldReturnConfigResponseWithEnvironment() {
+    when(getEnvironmentService.get()).thenReturn(EXPECTED_ENVIRONMENT);
+
+    final var response = informationController.getInformation();
+
+    assertEquals(EXPECTED_ENVIRONMENT, response.getEnvironment());
+  }
+
+  @Test
+  void shouldReturnConfigResponseWithBannersAndEnvironment() {
+
+    final var expectedResponse = InformationResponseDTO.builder()
+        .banners(EXPECTED_BANNERS)
+        .environment(EXPECTED_ENVIRONMENT)
+        .build();
+
+    when(getBannersService.get()).thenReturn(EXPECTED_BANNERS);
+    when(getEnvironmentService.get()).thenReturn(EXPECTED_ENVIRONMENT);
+
+    final var response = informationController.getInformation();
+
+    assertEquals(expectedResponse, response);
   }
 }
