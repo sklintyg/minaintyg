35d0be67ed4b9afd40f84dfe79794e7c15fd30e2
Daniel Jönsson <daniel.joensson@soprasteria.com>
2026-06-28 17:54:45 +0200

    K1J-1999: Moved JacksonConfig and removed log

--- app/src/main/java/se/inera/intyg/minaintyg/config/JacksonConfig.java
+++ app/src/main/java/se/inera/intyg/minaintyg/config/JacksonConfig.java
@@ -1,17 +1,14 @@
-package se.inera.intyg.minaintyg.integration.intygproxyservice.citizen.configuration;
+package se.inera.intyg.minaintyg.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.DeserializationFeature;
 
-@Slf4j
 @Configuration
 public class JacksonConfig {
   @Bean
   public JsonMapperBuilderCustomizer jacksonCustomizer() {
-    log.info("Jackson Customizer");
     return builder -> builder.disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
   }
 }
